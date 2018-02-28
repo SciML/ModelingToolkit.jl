1a2cfb958f529d0fa99aac8acae5988e71331eb9
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-02-28 13:20:36 -0800

    pass variable args

--- src/variables.jl
+++ src/variables.jl
@@ -5,11 +5,12 @@ struct Variable <: Real
     value_type::DataType
 end
 
+Variable(name,subtype,value,value_type = typeof(value)) = Variable(name,subtype,value,value_type)
 Variable(name,subtype) = Variable(name,subtype,nothing,Void)
-Variable(name) = Variable(name,:None)
-Parameter(name) = Variable(name,:Parameter)
+Variable(name,args...) = Variable(name,:None,args...)
+Parameter(name,args...) = Variable(name,:Parameter,args...)
 Constant(value) = Variable(:None,:Constant,value,typeof(value))
-DependentVariable(name) = Variable(name,:DependentVariable)
-IndependentVariable(name) = Variable(name,:IndependentVariable)
+DependentVariable(name,args...) = Variable(name,:DependentVariable,args...)
+IndependentVariable(name,args...) = Variable(name,:IndependentVariable,args...)
 
 export Variable,Parameter,Constant,DependentVariable,IndependentVariable
