6dc6e8976d5759041f2ec5939f885c85823f00ee
YingboMa <mayingbo5@gmail.com>
2018-03-03 19:38:55 -0800

    Add some macros to help define variables

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,24 @@
+export @dvars, @idvars, @paras
+
 function expr_arr_to_block(exprs)
   block = :(begin end)
   foreach(expr -> push!(block.args, expr), exprs)
   block
+end
+
+# Build variables more easily
+for funs in ((:dvars, :DependentVariable), (:idvars, :IndependentVariable),
+             (:paras, :Parameter))
+    @eval begin
+        macro ($(funs[1]))(x...)
+            ex = Expr(:block)
+            for var in x
+                @assert var isa Symbol "@$($funs[1]) expects a tuple of symbols!\nE.g. `@$($funs[1]) x y z`"
+                expr = :( $(esc(var)) = $($funs[2])( Symbol($(String(var))) ) )
+                push!(ex.args, expr)
+            end
+            push!(ex.args, Expr(:tuple, esc.(x)...))
+            ex
+        end
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,10 @@ using SciCompDSL
 using Base.Test
 
 # Define some variables
-x = DependentVariable(:x)
-y = DependentVariable(:y)
-z = DependentVariable(:z)
-t = IndependentVariable(:t)
+@dvars x y z
+@idvars t
 D = Differential(t) # Default of first derivative, Derivative(t,1)
-σ = Parameter(:σ)
-ρ = Parameter(:ρ)
-β = Parameter(:β)
+@paras σ ρ β
 c = Constant(0)
 σ*(y-x)
 D*x
