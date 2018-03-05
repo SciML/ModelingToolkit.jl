3f523d9e3cecb03c477de27138fa93ac0825176d
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-03-04 17:28:25 -0800

    nonlinear system handling

--- src/systems.jl
+++ src/systems.jl
@@ -14,21 +14,9 @@ function DiffEqSystem(eqs, ivs)
     DiffEqSystem(eqs, ivs, dvs, vs, ps)
 end
 
-
-
-struct NonlinearSystem <: AbstractSystem
-    eqs::Vector{Operation}
-    vs::Vector{Variable}
-    ps::Vector{Variable}
-end
-function NonlinearSystem(eqs)
-    vs, ps = extract_elements(eqs, (:DependentVariable, :Parameter))
-    NonlinearSystem(eqs, vs, ps)
-end
-
 function generate_ode_function(sys::DiffEqSystem)
     var_exprs = [:($(sys.dvs[i].name) = u[$i]) for i in 1:length(sys.dvs)]
-    param_exprs = [:($(sys.ps[i].name) = p[$i]) for i in 1:length(sys.dvs)]
+    param_exprs = [:($(sys.ps[i].name) = p[$i]) for i in 1:length(sys.ps)]
     sys_exprs = map(eq->:($(Symbol("$(eq.args[1].name)_$(eq.args[1].diff.x.name)")) = $(eq.args[2])),sys.eqs)
     dvar_exprs = [:(du[$i] = $(Symbol("$(sys.dvs[i].name)_$(sys.ivs[1].name)"))) for i in 1:length(sys.dvs)]
     exprs = vcat(var_exprs,param_exprs,sys_exprs,dvar_exprs)
@@ -41,4 +29,27 @@ function DiffEqBase.DiffEqFunction(sys::DiffEqSystem)
     DiffEqFunction{true}(eval(expr))
 end
 
-export DiffEqSystem,NonlinearSystem,DiffEqFunction
+struct NonlinearSystem <: AbstractSystem
+    eqs::Vector{Operation}
+    vs::Vector{Variable}
+    ps::Vector{Variable}
+end
+
+function NonlinearSystem(eqs)
+    # Allow the use of :DependentVariable to make it seamless with DE use
+    dvs, vs, ps = extract_elements(eqs, (:DependentVariable, :Variable, :Parameter))
+    vs = [dvs;vs]
+    NonlinearSystem(eqs, vs, ps)
+end
+
+function generate_nlsys_function(sys::NonlinearSystem)
+    var_exprs = [:($(sys.dvs[i].name) = u[$i]) for i in 1:length(sys.vs)]
+    param_exprs = [:($(sys.ps[i].name) = p[$i]) for i in 1:length(sys.ps)]
+    sys_exprs = [:($(Symbol("resid[i]")) = $(eq[i].args[2])) for i in eachindex(sys.eqs)]
+    exprs = vcat(var_exprs,param_exprs,sys_exprs)
+    block = expr_arr_to_block(exprs)
+    :((du,u,p)->$(block))
+end
+
+export DiffEqSystem, NonlinearSystem, DiffEqFunction
+export generate_ode_function, generate_nlsys_function
