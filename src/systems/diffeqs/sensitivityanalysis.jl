30f74a19ca0ba276af50327cf9662f7d93d9fa9d
Unknown <vaibhavyashdixit@gmail.com>
2018-03-22 21:39:13 +0530

    Rewrite functions to call calculate_jacobian instead of locally calculating the derivatives

--- src/systems/diffeqs/sensitivityanalysis.jl
+++ src/systems/diffeqs/sensitivityanalysis.jl
@@ -1,28 +1,17 @@
 function calculate_paramderivs(de::DiffEqSystem)
-    derivs = []
-    for i in 1:length(de.ps)
-        deriv_param = []
-        for j in 1:length(de.eqs)
-            @Deriv D'~de.ps[i]
-            deriv_eq = de.eqs[j].args[2]
-            deriv_eq = D*deriv_eq
-            push!(deriv_param,expand_derivatives(deriv_eq))
-        end
-        push!(derivs,deriv_param)
+    eqns = []
+    for i in de.eqs
+        push!(eqns,i.args[2])
     end
+    derivs = calculate_jacobian(eqns, de.ps)
     derivs
 end
 
 function calculate_dvarderivs(de::DiffEqSystem)
-    derivs = []
-    for i in 1:length(de.ps)
-        deriv_param = []
-        @Deriv D'~de.ps[i]
-        for j in 1:length(de.dvs)
-            l = DependentVariable(de.dvs[j].name,dependents=[de.ps[i]])
-            push!(deriv_param,D*l)
-        end
-        push!(derivs,deriv_param)
+    dvars = []
+    for i in de.dvars
+        push!(dvars,i)
     end
+    derivs = calculate_jacobian(dvars, de.ps)
     derivs
 end
