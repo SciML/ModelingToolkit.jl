41acf6f81f6047b52c45a7c70fd7e85e095255ba
Unknown <vaibhavyashdixit@gmail.com>
2018-03-25 19:15:39 +0530

    Updated calculation

--- src/systems/diffeqs/sensitivityanalysis.jl
+++ src/systems/diffeqs/sensitivityanalysis.jl
@@ -1,11 +1,16 @@
 calculate_paramderivs(de::DiffEqSystem) = calculate_jacobian(de,de.ps)
-calculate_dvarderivs(de::DiffEqSystem) = calculate_jacobian(de.dvs,de.ps)
+function calculate_dvarderivs(de::DiffEqSystem)
+    params = de.ps
+    dvars = de.dvs
+    new_params = [DependentVariable(i.name,dependents=de.ivs) for i in de.ps]
+    calculate_jacobian(dvars,new_params)
+end
 
 function get_sensitivity(de::DiffEqSystem)
     jac = calculate_jacobian(de)
     param_jac = calculate_paramderivs(de)
     dvar_jac = calculate_dvarderivs(de)
-    sensitivity = []
+    sensitivity = Operation[]
     for i in 1:length(de.ps)
         push!(sensitivity,jac*dvar_jac[i]+param_jac[i])
     end
