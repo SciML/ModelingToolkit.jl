7312a72d1923deb53b84aedde29b9190b205265c
Harrison Grodin <harrison.grodin@gmail.com>
2019-01-14 22:42:04 -0500

    Fix first-order transformation

--- test/system_construction.jl
+++ test/system_construction.jl
@@ -38,7 +38,7 @@ test_vars_extraction(de, de2)
 eqs = [D3(u) ~ 2(D2(u)) + D(u) + D(x) + 1
        D2(x) ~ D(x) + 2]
 de = DiffEqSystem(eqs, [t])
-@test_broken de1 = ode_order_lowering(de)
+de1 = ode_order_lowering(de)
 lowered_eqs = [D(u_tt) ~ 2u_tt + u_t + x_t + 1
                D(x_t)  ~ x_t + 2
                D(u_t)  ~ u_tt
@@ -57,7 +57,7 @@ function test_eqs(eqs1, eqs2)
     end
     eq
 end
-@test_broken test_eqs(de1.eqs, lowered_eqs)
+@test test_eqs(de1.eqs, lowered_eqs)
 
 # Internal calculations
 a = y - x
