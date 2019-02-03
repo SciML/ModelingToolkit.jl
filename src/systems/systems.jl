e69c96226cd1ffad6322942f5dd7ddb11298e0f1
Harrison Grodin <harrison.grodin@gmail.com>
2019-02-02 22:43:15 -0500

    Use build_function in generate_jacobian

--- src/systems/systems.jl
+++ src/systems/systems.jl
@@ -7,15 +7,10 @@ function system_eqs    end
 function system_vars   end
 function system_params end
 
-function generate_jacobian(sys::AbstractSystem)
+function generate_jacobian(sys::AbstractSystem; version = ArrayFunction)
     vs, ps = system_vars(sys), system_params(sys)
-    var_exprs = [:($(vs[i].name) = u[$i]) for i in eachindex(vs)]
-    param_exprs = [:($(ps[i].name) = p[$i]) for i in eachindex(ps)]
     jac = calculate_jacobian(sys)
-    jac_exprs = [:(J[$i,$j] = $(convert(Expr, jac[i,j]))) for i in 1:size(jac,1), j in 1:size(jac,2)]
-    exprs = vcat(var_exprs, param_exprs, vec(jac_exprs))
-    block = expr_arr_to_block(exprs)
-    :((J,u,p,t) -> $(block))
+    return build_function(jac, vs, ps, (:t,); version = version)
 end
 
 function generate_function(sys::AbstractSystem; version::FunctionVersion = ArrayFunction)
