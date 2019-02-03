285e5825818ced16ed5b4dab791988aa11222a49
Harrison Grodin <harrison.grodin@gmail.com>
2019-02-03 01:30:07 -0500

    Remove internal tests
    Only user-facing API should be tested.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,6 @@ using ModelingToolkit, Test
 @testset "Parsing Test" begin include("variable_parsing.jl") end
 @testset "Basic Variables and Operations" begin include("basic_variables_and_operations.jl") end
 @testset "Differentiation Test" begin include("derivatives.jl") end
-@testset "Internal Test" begin include("internal.jl") end
 @testset "Simplify Test" begin include("simplify.jl") end
 @testset "Ambiguity Test" begin include("ambiguity.jl") end
 @testset "Components Test" begin include("components.jl") end
