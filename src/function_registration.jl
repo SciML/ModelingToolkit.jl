99293ffbbfff9ec7047c12a7af645bdf8cacd45a
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-12-26 08:04:00 -0500

    Fix rational issues and Latexify tests
    fixes https://github.com/JuliaDiffEq/ModelingToolkit.jl/issues/216

--- src/function_registration.jl
+++ src/function_registration.jl
@@ -54,6 +54,7 @@ end
 
 # special cases
 Base.:^(x::Expression,y::T) where T <: Integer = Operation(Base.:^, Expression[x, y])
+Base.:^(x::Expression,y::T) where T <: Rational = Operation(Base.:^, Expression[x, y])
 
 @register Base.getindex(x,i)
 Base.getindex(x::Operation,i::Int64) = Operation(getindex,[x,i])
