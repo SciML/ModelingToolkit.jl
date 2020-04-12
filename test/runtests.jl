3a34965dec98dcbdaa819e7a674db6c4a0780336
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-04-12 01:36:36 -0400

    Setup ReactionSystem on AbstractSystem interface

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,8 @@ using SafeTestsets, Test
 @safetestset "Mass Matrix Test" begin include("mass_matrix.jl") end
 @safetestset "SDESystem Test" begin include("sdesystem.jl") end
 @safetestset "NonlinearSystem Test" begin include("nonlinearsystem.jl") end
-@safetestset "OptimizationSystem Test" begin include("optimizationsystem.jl") end
+@safetestset "OptimizationSystem Test" begin include("optimizationsystem.jl") en
+@safetestset "ReactionSystem Test" begin include("reactionsystem.jl") end
 @safetestset "Build Targets Test" begin include("build_targets.jl") end
 @safetestset "Domain Test" begin include("domains.jl") end
 @safetestset "Constraints Test" begin include("constraints.jl") end
