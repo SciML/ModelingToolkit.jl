c7baf80cd78a53026cf537aa09c5ea845c4762d9
Shashi Gowda <gowda@mit.edu>
2020-07-06 14:16:27 -0400

    speed up izero

--- src/operations.jl
+++ src/operations.jl
@@ -58,7 +58,10 @@ Base.hash(o::Operation, salt::UInt) = hash(o.args, hash(o.op, salt))
 
 # provide iszero for Operations to help sparse addition and multiplication
 # e.g., we want to tell the sparse library that iszero(zero(Operation) + zero(Operation)) == true
-Base.iszero(x::Operation) = (_x = simplify(x); _x isa Constant && iszero(_x.value))
+function Base.iszero(x::Operation)
+    _x = SymbolicUtils.polynormalize(to_symbolic(x))
+   return !(_x isa Number) ? false : iszero(_x)
+end
 
 Base.show(io::IO, O::Operation) = print(io, convert(Expr, O))
 
