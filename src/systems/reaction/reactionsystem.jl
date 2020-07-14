b7884391c8875c1a1a17bbaa253806e412e4f2a4
Samuel Isaacson <isaacsas@users.noreply.github.com>
2020-07-14 13:33:50 -0400

    tweak comment

--- src/systems/reaction/reactionsystem.jl
+++ src/systems/reaction/reactionsystem.jl
@@ -163,9 +163,9 @@ the `Operation` that is returned will be `k * (X(t)^2/2) * (Y(t)^3/6)`.
 Notes:
 - Allocates
 - `combinatoric_ratelaw=true` uses factorial scaling factors in calculating the rate
-law, i.e. for `2S -> 0` at rate `k` the ratelaw would be `k*S^2/2!`. If
-`combinatoric_ratelaw=false` then the ratelaw is `k*S^2`, i.e. the scaling factor is 
-ignored.
+    law, i.e. for `2S -> 0` at rate `k` the ratelaw would be `k*S^2/2!`. If
+    `combinatoric_ratelaw=false` then the ratelaw is `k*S^2`, i.e. the scaling factor is 
+    ignored.
 """ 
 function oderatelaw(rx; combinatoric_ratelaw=true)
     @unpack rate, substrates, substoich, only_use_rate = rx
