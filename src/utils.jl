5091216d7d05d78bb6e747d7d4e6c9a4f7b5bccc
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-12-01 04:33:17 -0500

    fix dispatch signature

--- src/utils.jl
+++ src/utils.jl
@@ -271,10 +271,10 @@ function lower_varname(t::Term, iv)
 end
 lower_varname(t::Sym, iv) = t
 
-function lower_mapnames(umap::AbstractArray{<:Pair{Symbol,T}}) where T
+function lower_mapnames(umap::AbstractArray{<:Pair}) where T
     [value(k) => value(v) for (k, v) in umap]
 end
-function lower_mapnames(umap::AbstractArray{<:Pair{Symbol,T}},name) where T
+function lower_mapnames(umap::AbstractArray{<:Pair},name) where T
     [lower_varname(value(k), name) => value(v) for (k, v) in umap]
 end
 lower_mapnames(umap::AbstractArray{<:Number}) = umap # Ambiguity
