70cd4aecb0b22ace9381ac881d9f719e40d36612
Niklas Korsbo <niklas@pumas.ai>
2020-12-23 13:27:31 +0100

    Increase readability of Latexify code.

--- src/latexify_recipes.jl
+++ src/latexify_recipes.jl
@@ -5,30 +5,30 @@ Convert equations to prettified expressions for latexification.
 """
 function pretty_expr end
 
-pretty_expr(expr; kwargs...) = expr
-pretty_expr(s::Sym; kwargs...) = nameof(s)
-pretty_expr(eq::Equation; kwargs...) = Expr(:(=), pretty_expr(eq.lhs; kwargs...), pretty_expr(eq.rhs; kwargs...))
-pretty_expr(eq::AbstractArray; kwargs...) = pretty_expr.(eq; kwargs...)
-pretty_expr(x::Integer; kwargs...) = x
-pretty_expr(x::AbstractFloat; kwargs...) = x
-pretty_expr(x::Num; kwargs...) = pretty_expr(value(x); kwargs...)
-pretty_expr(expr::Expr; kwargs...) = Expr(expr.head, pretty_expr.(expr.args; kwargs...)...)
-pretty_expr(f::Function; kwargs...) = nameof(f)
-pretty_expr(f::Term; kwargs...) = pretty_expr(f.f, f.args; kwargs...)
+# single-argument
+pretty_expr(x; kw...) = x
+pretty_expr(s::Sym; kw...) = nameof(s)
+pretty_expr(f::Function; kw...) = nameof(f)
+pretty_expr(x::Num; kw...) = pretty_expr(value(x); kw...)
+pretty_expr(ex::Expr; kw...) = Expr(ex.head, pretty_expr.(ex.args; kw...)...)
+pretty_expr(eq::Equation; kw...) = Expr(:(=), pretty_expr(eq.lhs; kw...), pretty_expr(eq.rhs; kw...))
+pretty_expr(f::Term; kw...) = pretty_expr(f.f, f.args; kw...)
 
-pretty_expr(f, args; kwargs...) = Expr(:call, pretty_expr(f; kwargs...), pretty_expr.(args; kwargs...)...)
-pretty_expr(f::Function, args; kwargs...) = Expr(:call, nameof(f), pretty_expr.(args; kwargs...)...)
+# double-argument
+pretty_expr(f, args; kw...) = Expr(:call, pretty_expr(f; kw...), pretty_expr.(args; kw...)...)
+pretty_expr(f::Function, args; kw...) = Expr(:call, nameof(f), pretty_expr.(args; kw...)...)
 
-function pretty_expr(f::Sym, args; show_iv=false, var_syms=false, kwargs...)
+function pretty_expr(f::Sym, args; show_iv=false, var_syms=false, kw...)
     isempty(args) && return f.name
-    if show_iv
+    if show_iv # show independent variable?
+        ## squish the variables and independent variable into a symbol to avoid being interpreted as a function call in latexify?
         var_syms ? Symbol(pretty_expr(f), "(", join(pretty_expr.(args), ","), ")") : Expr(:call, pretty_expr(f), pretty_expr.(args)...)
     else
-        return f.name
+        f.name
     end
 end
 
-function pretty_expr(f::Differential, args; kwargs...)
+function pretty_expr(f::Differential, args; kw...)
     ## higher-order derivative?
     if hasfield(typeof(args[1]), :f) && args[1].f isa Differential
         arg, diffs = incept_derivative(f, args, Symbol[])
@@ -36,12 +36,19 @@ function pretty_expr(f::Differential, args; kwargs...)
         exponents = [count(==(x), diffs) for x in diffvars]
         return Expr(:call, 
             :/, 
-            Expr(:latexifymerge, "d^{$(length(diffs))}", pretty_expr(arg; kwargs...)), 
+            Expr(:latexifymerge, "d^{$(length(diffs))}", pretty_expr(arg; kw...)), 
             join(Symbol.(:d, string.(diffvars) .* "^{", string.(exponents), "}"))
             )
     end
-    Expr(:call, :/, Expr(:latexifymerge, :d, pretty_expr(args[1]; kwargs...)), Symbol(:d, f.x))
+    Expr(:call, :/, Expr(:latexifymerge, :d, pretty_expr(args[1]; kw...)), Symbol(:d, f.x))
 end
+
+"""
+    incept_derivative
+
+recursively figure out what's being derivated and with respect to what.
+"""
+function incept_derivative end
 incept_derivative(f::Differential, args, diffs) = incept_derivative(args[1], vcat(diffs, f.x))
 incept_derivative(x::Term, diffs) = incept_derivative(x.f, x.args, diffs)
 incept_derivative(f, args, diffs) = (Term(f, args), diffs)
@@ -50,9 +57,6 @@ incept_derivative(f, args, diffs) = (Term(f, args), diffs)
     # Set default option values.
     env --> :align
     cdot --> false
-
-    # Convert both the left- and right-hand sides to expressions of basic types
-    # that latexify can deal with
 
     rhs = getfield.(eqs, :rhs)
     rhs = pretty_expr.(rhs; show_iv=show_iv, var_syms=var_syms)
--- test/latexify.jl
+++ test/latexify.jl
@@ -107,7 +107,6 @@ raw"\begin{align}
 @variables x(t)
 @derivatives D'~t
 eqs = [D(x) ~ (1+cos(t))/(1+2*x)]
-eqs[1].rhs.args[1].args[2].f isa Function
 
 # Latexify.@generate_test latexify(eqs)
 @test latexify(eqs) == replace(
