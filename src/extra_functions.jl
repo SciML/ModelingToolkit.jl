7f5e0abfdf941c49b1df9ab5ced1043f946381c8
Yingbo Ma <mayingbo5@gmail.com>
2020-12-23 20:31:46 -0500

    Add special rules for max and min

--- src/extra_functions.jl
+++ src/extra_functions.jl
@@ -4,8 +4,23 @@
 
 @register Base.signbit(x)
 ModelingToolkit.derivative(::typeof(signbit), args::NTuple{1,Any}, ::Val{1}) = 0
-
 ModelingToolkit.derivative(::typeof(abs), args::NTuple{1,Any}, ::Val{1}) = IfElse.ifelse(signbit(args[1]),-one(args[1]),one(args[1]))
+function ModelingToolkit.derivative(::typeof(min), args::NTuple{2,Any}, ::Val{1})
+    x, y = args
+    IfElse.ifelse(x < y, one(x), zero(x))
+end
+function ModelingToolkit.derivative(::typeof(min), args::NTuple{2,Any}, ::Val{2})
+    x, y = args
+    IfElse.ifelse(x < y, zero(y), one(y))
+end
+function ModelingToolkit.derivative(::typeof(max), args::NTuple{2,Any}, ::Val{1})
+    x, y = args
+    IfElse.ifelse(x > y, one(x), zero(x))
+end
+function ModelingToolkit.derivative(::typeof(max), args::NTuple{2,Any}, ::Val{2})
+    x, y = args
+    IfElse.ifelse(x > y, zero(y), one(y))
+end
 
 @register IfElse.ifelse(x,y,z::Any)
 ModelingToolkit.derivative(::typeof(IfElse.ifelse), args::NTuple{3,Any}, ::Val{1}) = 0
