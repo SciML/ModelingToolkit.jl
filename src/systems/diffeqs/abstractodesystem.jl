6dd68901509dae027007d61847affc4a1ad912f1
Yingbo Ma <mayingbo5@gmail.com>
2021-01-30 18:03:03 -0500

    Add indepsym

--- src/systems/diffeqs/abstractodesystem.jl
+++ src/systems/diffeqs/abstractodesystem.jl
@@ -166,12 +166,15 @@ function DiffEqBase.ODEFunction{iip}(sys::AbstractODESystem, dvs = states(sys),
 
     _M = (u0 === nothing || M == I) ? M : ArrayInterface.restructure(u0 .* u0',M)
 
-    ODEFunction{iip}(f,
+    ODEFunction{iip}(
+                     f,
                      jac = _jac === nothing ? nothing : _jac,
                      tgrad = _tgrad === nothing ? nothing : _tgrad,
                      mass_matrix = _M,
                      jac_prototype = sparse ? similar(sys.jac[],Float64) : nothing,
-                     syms = Symbol.(states(sys)))
+                     syms = Symbol.(states(sys)),
+                     indepsym = Symbol(independent_variable(sys)),
+                    )
 end
 
 """
@@ -227,12 +230,15 @@ function ODEFunctionExpr{iip}(sys::AbstractODESystem, dvs = states(sys),
         tgrad = $_tgrad
         jac = $_jac
         M = $_M
-        ODEFunction{$iip}(f,
-                         jac = jac,
-                         tgrad = tgrad,
-                         mass_matrix = M,
-                         jac_prototype = $jp_expr,
-                         syms = $(Symbol.(states(sys))))
+        ODEFunction{$iip}(
+                          f,
+                          jac = jac,
+                          tgrad = tgrad,
+                          mass_matrix = M,
+                          jac_prototype = $jp_expr,
+                          syms = $(Symbol.(states(sys))),
+                          indepsym = $(QuoteNode(Symbol(independent_variable(sys)))),
+                         )
     end
     !linenumbers ? striplines(ex) : ex
 end
