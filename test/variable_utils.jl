3329dfef04d70a930937b429fc331cca86a64fb7
Yingbo Ma <mayingbo5@gmail.com>
2021-02-02 20:10:15 -0500

    update tests

--- test/variable_utils.jl
+++ test/variable_utils.jl
@@ -1,10 +1,12 @@
 using ModelingToolkit, Test
+using ModelingToolkit: value
 using SymbolicUtils: <ₑ
 @parameters α β δ
 expr = (((1 / β - 1) + δ) / α) ^ (1 / (α - 1))
 ref = sort([β, δ, α], lt = <ₑ)
 sol = sort(Num.(ModelingToolkit.get_variables(expr)), lt = <ₑ)
-@test all(simplify, sol[i] == ref[i] for i in 1:3)
+@test all(x->x isa Num, sol[i] == ref[i] for i in 1:3)
+@test all(simplify∘value, sol[i] == ref[i] for i in 1:3)
 
 @parameters γ
 s = α => γ
