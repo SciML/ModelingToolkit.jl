7484fc5529fa01ebf2417ac5f49ff66ca634b90f
Kirill Zubov <Kirillzubov3@gmail.com>
2021-02-05 16:49:12 +0300

    Update pdesystem.jl

--- src/systems/pde/pdesystem.jl
+++ src/systems/pde/pdesystem.jl
@@ -1,17 +1,14 @@
-struct PDESystem{iip}
+struct PDESystem
   eq
   bcs
   domain
   indvars
   depvars
 
-  @add_kwonly function PDESystem{iip}(eq, bcs, domain, indvars, depvars) where iip
-      new{iip}(eq, bcs, domain, indvars, depvars)
+  @add_kwonly function PDESystem(eq, bcs, domain, indvars, depvars) 
+      new(eq, bcs, domain, indvars, depvars)
   end
 end
 
-PDESystem(args...) = PDESystem{true}(args...)
-
-SciMLBase.isinplace(prob::PDESystem{iip}) where iip = iip
 
 
