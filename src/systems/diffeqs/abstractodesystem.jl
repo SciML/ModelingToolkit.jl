dd5327addb025440d2c97d9e4326f2073d16b72a
anand <anandj@uchicago.edu>
2021-02-26 14:48:07 -0800

    @named start

--- src/systems/diffeqs/abstractodesystem.jl
+++ src/systems/diffeqs/abstractodesystem.jl
@@ -420,3 +420,22 @@ end
 
 isdifferential(expr) = istree(expr) && operation(expr) isa Differential
 isdiffeq(eq) = isdifferential(eq.lhs)
+
+"""
+named(ex):
+
+Used for naming an AbstractODESystem from an expression of the form `var = value`.
+A keyword argument (name = :var) is added as the first keyword argument.
+
+example:
+
+`@named sys = ODESystem(eqs, args...; kwargs...)`
+
+"""
+macro named(ex)
+    if !(ex isa Expr && ex.head == :(=))
+      throw(ArgumentError("expression should be of the form `var = value`"))
+    end
+    pushfirst!(ex.args[2].args[2].args, :(name = :foo))
+    :($(esc(ex.args[1])) = $(esc(ex.args[2])))
+end
