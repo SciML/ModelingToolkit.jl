6c6d5602b967933c1f8ebd978aa476eb93912afe
Mohamed Tarek <mohamed82008@gmail.com>
2021-03-04 13:41:28 +1100

    serialization test

---
+++ test/serialization.jl
@@ -0,0 +1,21 @@
+using ModelingToolkit, SciMLBase, Serialization
+
+@parameters t
+@variables x(t)
+D = Differential(t)
+
+sys = ODESystem([D(x) ~ -0.5*x])
+for prob in [
+    eval(ModelingToolkit.ODEProblem{false}(sys, nothing, nothing, SciMLBase.NullParameters())),
+    eval(ModelingToolkit.ODEProblemExpr{false}(sys, nothing, nothing, SciMLBase.NullParameters()))
+]
+    _fn = tempname()
+
+    open(_fn, "w") do f
+        serialize(f, prob)
+    end
+
+    _cmd = "using ModelingToolkit, Serialization; deserialize(\"$_fn\")"
+
+    run(`$(Base.julia_cmd()) --project -e $(_cmd)`)
+end
