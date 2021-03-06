f184bfd441d7dc258ada8624465589126999f6e0
Yingbo Ma <mayingbo5@gmail.com>
2021-03-06 16:35:41 -0500

    Handle mass matrix in modelingtoolkitize

--- src/systems/diffeqs/modelingtoolkitize.jl
+++ src/systems/diffeqs/modelingtoolkitize.jl
@@ -18,19 +18,29 @@ function modelingtoolkitize(prob::DiffEqBase.ODEProblem)
     vars = ArrayInterface.restructure(prob.u0,[var(:x, i)(ModelingToolkit.value(t)) for i in eachindex(prob.u0)])
     params = p isa DiffEqBase.NullParameters ? [] :
              reshape([Num(Sym{Real}(nameof(Variable(:α, i)))) for i in eachindex(p)],size(p))
+    var_set = Set(vars)
 
     D = Differential(t)
+    mm = prob.f.mass_matrix
 
-    rhs = [D(var) for var in vars]
-
-    if DiffEqBase.isinplace(prob)
-        lhs = similar(vars, Num)
-        prob.f(lhs, vars, params, t)
-    else
-        lhs = prob.f(vars, params, t)
+    lhs = map(mm * vars) do v
+        if iszero(v)
+            0
+        elseif v in var_set
+            D(v)
+        else
+            error("Non-permuation mass matrix is not supported.")
+        end
     end
 
-    eqs = vcat([rhs[i] ~ lhs[i] for i in eachindex(prob.u0)]...)
+    if DiffEqBase.isinplace(prob)
+        rhs = similar(vars, Num)
+        prob.f(rhs, vars, params, t)
+    else
+        rhs = prob.f(vars, params, t)
+    end
+
+    eqs = vcat([lhs[i] ~ rhs[i] for i in eachindex(prob.u0)]...)
 
     params = if ndims(params) == 0
         [params[1]]
