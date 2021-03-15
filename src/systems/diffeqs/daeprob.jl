fbd61cea2713cb416274866bb0399aee2d9588d6
Anand <anandj@uchicago.edu>
2021-03-15 16:37:48 -0700

    tmp

--- src/systems/diffeqs/daeprob.jl
+++ src/systems/diffeqs/daeprob.jl
@@ -1,43 +1,11 @@
-# WIP 
+using ModelingToolkit, DiffEqBase, OrdinaryDiffEq, DifferentialEquations
 
-# "The goal is to make a DAEProblem and DAEFunction constructor"
-#    - constructors that take sys::AbstractODESystem though,
-#  since there are already constructors that take an f(du, u, p, t) or f!(out, du, u, p, t)
-
-# scratchpad
-# using ModelingToolkit, DiffEqBase, OrdinaryDiffEq, DifferentialEquations
-
-# function f(out,du,u,p,t)
-#   out[1] = - 0.04u[1]              + 1e4*u[2]*u[3] - du[1]
-#   out[2] = + 0.04u[1] - 3e7*u[2]^2 - 1e4*u[2]*u[3] - du[2]
-#   out[3] = u[1] + u[2] + u[3] - 1.0
-# end
-
-# u₀ = [1.0, 0, 0]
-# du₀ = [-0.04, 0.04, 0.0]
-# tspan = (0.0,100.0)
-
-# differential_vars = [true,true,false]
-# prob = DAEProblem(f,du₀,u₀,tspan,differential_vars=differential_vars)
-# solve(prob, Tsit5()) # ERROR: You cannot use an ODE Algorithm with a DAEProblem
-# sys = modelingtoolkitize(prob) # fails
-# sol = solve(prob) # figures out it needs a DAE alg
-
-
-# @parameters t
-# @variables u[1:3](t)
-# D = Differential(t)
-# # symbolic representation of rober
-# e1 = D(u[1]) ~ -0.04u[1] + 1e4 * u[2] * u[3]
-# e2 = D(u[2]) ~ 0.04u[1] - 3e7 * u[2]^2 - 1e4 * u[2] * u[3]
-# e3 = 1.0 ~ u[1] + u[2] + u[3]
-
-# eqs = [e1, e2, e3]
-
-
-function eq_to_num(eq::Equation)::Num
-    getfield(eq, :rhs) - getfield(eq, :lhs)
+function eq_to_num(eq::Equation)
+    lhs(eq) - rhs(eq)
 end
+
+lhs(eq::Equation) = getfield(eq, :lhs)
+rhs(eq::Equation) = getfield(eq, :rhs)
 
 """
     implicitize(eq::Equation)::Equation
@@ -45,8 +13,43 @@ end
 transform an equation of the form y = f(x) 
 into f(x) - y = 0
 """
-function implicitize(eq::Equation)::Equation
+function implicitize(eq::Equation)
     eq_to_num(eq) ~ 0 
 end
 
-# ieqs = implicitize.(eqs)
+@parameters t r
+@variables u[1:3](t)
+D = Differential(t)
+# symbolic representation of rober
+eqs = [
+    D(u[1]) ~ -u[1] + r * u[2] * u[3],
+    D(u[2]) ~ u[1] - u[2]^2 - u[2] * u[3],
+    1.0 ~ u[1] + u[2] + u[3]
+]
+nums = eq_to_num.(eqs)
+@variables du[1:3] # temporaries instead of D.(u)
+p = [r]
+f_expr = build_function(nums, du, u, p, t) # du,u,p,t
+fiip = f_expr[2]
+
+myf = eval(fiip)
+
+out = zeros(3)
+u₀ = [1.0, 0, 0]
+du₀ = [-0.04, 0.04, 0.0]
+tspan = (0.0, 100.0)
+ps = rand(1)
+myf(out,du0,u0,ps, tspan)
+
+Base.remove_linenums!(fiip)
+
+:(function (var"##out#264", var"##arg#261", var"##arg#262", var"##arg#263", t)
+      let du₁ = var"##arg#261"[1], du₂ = var"##arg#261"[2], du₃ = var"##arg#261"[3], var"u₁(t)" = var"##arg#262"[1], var"u₂(t)" = var"##arg#262"[2], var"u₃(t)" = var"##arg#262"[3], r = var"##arg#263"[1]
+          #= C:\Users\Anand\.julia\packages\SymbolicUtils\V6S2E\src\code.jl:310 =# @inbounds begin
+                  var"##out#264"[1] = (+)((*)(-1, r, var"u₂(t)", var"u₃(t)"), var"u₁(t)", (Differential(t))(var"u₁(t)"))
+                  var"##out#264"[2] = (+)((Differential(t))(var"u₂(t)"), (*)(-1, var"u₁(t)"), (^)(var"u₂(t)", 2), (*)(var"u₂(t)", var"u₃(t)"))
+                  var"##out#264"[3] = (+)(1.0, (*)(-1, var"u₁(t)"), (*)(-1, var"u₂(t)"), (*)(-1, var"u₃(t)"))
+                  nothing
+              end
+      end
+  end)
