0ce69fe5545b6e08cf8a07095cebcd144c23c8f0
Yingbo Ma <mayingbo5@gmail.com>
2021-04-06 17:33:03 -0400

    Update simplify test

--- test/simplify.jl
+++ test/simplify.jl
@@ -20,7 +20,7 @@ simplify(minus_op)
 
 @variables x
 
-@test toexpr(expand_derivatives(Differential(x)((x-2)^2))) == :($(*)(2, $(+)(-2, x)))
+@test toexpr(expand_derivatives(Differential(x)((x-2)^2))) == :($(+)(-4, $(*)(2, x)))
 @test toexpr(expand_derivatives(Differential(x)((x-2)^3))) == :($(*)(3, $(^)($(+)(-2, x), 2)))
 @test toexpr(simplify(x+2+3)) == :($(+)(5, x))
 
