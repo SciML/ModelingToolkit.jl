c46381bdfb445134cc0f9689b86924d6422c55aa
anand jain <anandj@uchicago.edu>
2021-04-23 14:21:57 -0700

    fix

--- src/systems/abstractsystem.jl
+++ src/systems/abstractsystem.jl
@@ -739,5 +739,5 @@ function connect(syss...)
 end
 
 # idk where to put them 
-Base.write(io::IO, sys::T) where {T <: ModelingToolkit.AbstractODESystem} = write(io, string(toexpr(sys)))
-Base.read(io::IO, sys::T) where {T <: ModelingToolkit.AbstractODESystem} = eval(Meta.parse(read(io, String)))
+Base.write(io::IO, sys::T) where {T <: AbstractODESystem} = write(io, string(toexpr(sys)))
+Base.read(io::IO, sys::T) where {T <: AbstractODESystem} = eval(Meta.parse(read(io, String)))
