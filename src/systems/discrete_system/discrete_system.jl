e0c6032894219f526029bbe93301631c1a090bed
Christopher Rackauckas <accounts@chrisrackauckas.com>
2021-04-29 21:48:14 -0400

    Update discrete_system.jl

--- src/systems/discrete_system/discrete_system.jl
+++ src/systems/discrete_system/discrete_system.jl
@@ -85,7 +85,11 @@ end
 
 Generates an DiscreteProblem from an DiscreteSystem.
 """
-function DiffEqBase.DiscreteProblem(sys::DiscreteSystem,u0map,tspan,parammap=DiffEqBase.NullParameters();kwargs...)
+function DiffEqBase.DiscreteProblem(sys::DiscreteSystem,u0map,tspan,
+                                    parammap=DiffEqBase.NullParameters();
+                                    eval_module = @__MODULE__,
+                                    eval_expression = true,
+                                    kwargs...)
     dvs = states(sys)
     ps = parameters(sys)
     eqs = equations(sys)
@@ -95,10 +99,9 @@ function DiffEqBase.DiscreteProblem(sys::DiscreteSystem,u0map,tspan,parammap=Dif
     rhss = [eq.rhs for eq in eqs]
     u = dvs
     p = varmap_to_vars(parammap,ps)
-    eval_module = @__MODULE__
-    eval_expression = true
+
     f_gen = build_function(rhss, dvs, ps, t; expression=Val{eval_expression}, expression_module=eval_module)
     f_oop,f_iip = (@RuntimeGeneratedFunction(eval_module, ex) for ex in f_gen)
     f(u,p,t) = f_oop(u,p,t)
-    DiscreteProblem(f,u0,tspan,p)
+    DiscreteProblem(f,u0,tspan,p;kwargs...)
 end
