4cc3dc7e1c6a42470915562ffd0ab7456fa03ba5
OvidioLe85 <ovile27@gmail.com>
2021-04-30 05:13:04 +0200

    DiscreteSystem (#986)
    * DiscreteSystem added
    
    * Update test/test_epidemiological_compartmental.jl
    
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>
    
    * Update test/test_epidemiological_compartmental.jl
    
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>
    
    * Update test/test_epidemiological_compartmental.jl
    
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>
    
    * Update test/test_epidemiological_compartmental.jl
    
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>
    
    * DiscreteSystem doesn't need two files
    
    * Rename test_epidemiological_compartmental.jl to discretesystem.jl
    
    * Update runtests.jl
    
    * Update discretesystem.jl
    
    * Update discrete_system.jl
    
    * Update discretesystem.jl
    
    * Update discretesystem.jl
    
    * Update discretesystem.jl
    
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using SafeTestsets, Test
 @safetestset "Simplify Test" begin include("simplify.jl") end
 @safetestset "Direct Usage Test" begin include("direct.jl") end
 @safetestset "System Linearity Test" begin include("linearity.jl") end
+@safetestset "DiscreteSystem Test" begin include("discretesystem.jl") end
 @safetestset "ODESystem Test" begin include("odesystem.jl") end
 @safetestset "LabelledArrays Test" begin include("labelledarrays.jl") end
 @safetestset "Mass Matrix Test" begin include("mass_matrix.jl") end
