fd5e27361d851dbb699fd7d8a5683a04469aea8d
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-05-23 20:14:49 -0400

    update tests

--- test/domains.jl
+++ test/domains.jl
@@ -1,5 +1,4 @@
-using ModelingToolkit
-using DomainSets
+using ModelingToolkit, DomainSets, Test
 
 domain = Interval(0, 1)
 @test infimum(domain) == 0
@@ -9,29 +8,18 @@ domain = -1.0..2.0
 @test infimum(domain) == -1.0
 @test supremum(domain) == 2.0
 
-domain = Ball()
+ball = Ball()
 @test radius(ball) == 1.0
 @test center(ball) == [0.0,0.0,0.0]
 
-domain = Ball(2.5, [1,2,3])
+ball = Ball(2.5, [1,2])
 @test radius(ball) == 2.5
-@test center(ball) == [1,2,3]
+@test center(ball) == [1,2]
 
 @parameters t x
 domains = [t ∈ Interval(0.0,1.0),
            x ∈ Interval(0.0,1.0)]
 
-@parameters z
-z ∈ (Interval(0.0,1.0) ⊗ Interval(0.0,1.0))
-
-@parameters y
-(x,y) ∈ CircleDomain()
-@parameters r θ
-(r,θ) ∈ CircleDomain(true)
-
-(x,y) ∈ UnitDisk()
-(r,θ) ∈ UnitDisk()
-
-(x,y,z) ∈ UnitBall()
-@parameters ϕ
-(r,θ,ϕ) ∈ UnitBall()
+@parameters y, z
+(x,y) ∈ Ball(2.0, [0,0])
+(x,y,z) ∈ Ball(1.5, [1,2,3])
