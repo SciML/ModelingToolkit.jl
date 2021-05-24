8ccc204e79177f479a169540193d22598027879c
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-05-23 20:02:40 -0400

    use new Ball()

--- src/domains.jl
+++ src/domains.jl
@@ -1,28 +1,11 @@
 using DomainSets
-
-abstract type AbstractDomain{T,N} <: Domain{T} end
-
 struct VarDomainPairing
   variables
   domain::Domain
 end
 Base.:∈(variable::ModelingToolkit.Num,domain::Domain) = VarDomainPairing(value(variable),domain)
+Base.:∈(variable::ModelingToolkit.Num,domain::Interval) = VarDomainPairing(value(variable),domain)
 Base.:∈(variables::NTuple{N,ModelingToolkit.Num},domain::Domain) where N = VarDomainPairing(value.(variables),domain)
 
-## Specific Domains
-
-struct IntervalDomain{T} <: AbstractDomain{T,1}
-  lower::T
-  upper::T
-end
-
-
-struct ProductDomain{D,T,N} <: AbstractDomain{T,N}
-  domains::D
-end
-⊗(args::AbstractDomain{T}...) where T = ProductDomain{typeof(args),T,length(args)}(args)
-
-struct CircleDomain <: AbstractDomain{Float64,2}
-  polar::Bool
-  CircleDomain(polar=false) = new(polar)
-end
+@deprecate IntervalDomain(a,b) Interval(a,b)
+@deprecate CircleDomain() Ball()
--- test/domains.jl
+++ test/domains.jl
@@ -1,12 +1,28 @@
 using ModelingToolkit
 using DomainSets
 
+domain = Interval(0, 1)
+@test infimum(domain) == 0
+@test supremum(domain) == 1
+
+domain = -1.0..2.0
+@test infimum(domain) == -1.0
+@test supremum(domain) == 2.0
+
+domain = Ball()
+@test radius(ball) == 1.0
+@test center(ball) == [0.0,0.0,0.0]
+
+domain = Ball(2.5, [1,2,3])
+@test radius(ball) == 2.5
+@test center(ball) == [1,2,3]
+
 @parameters t x
-domains = [t ∈ IntervalDomain(0.0,1.0),
-           x ∈ IntervalDomain(0.0,1.0)]
+domains = [t ∈ Interval(0.0,1.0),
+           x ∈ Interval(0.0,1.0)]
 
 @parameters z
-z ∈ (IntervalDomain(0.0,1.0) ⊗ IntervalDomain(0.0,1.0))
+z ∈ (Interval(0.0,1.0) ⊗ Interval(0.0,1.0))
 
 @parameters y
 (x,y) ∈ CircleDomain()
