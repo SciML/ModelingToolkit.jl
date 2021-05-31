cba26c1a9506b29c86e883f395c74396eb7fbd43
Valentin Sulzer <valentinsulzer@hotmail.com>
2021-05-31 14:23:44 -0400

    type piracy on Interval.lower/upper for downstream compat

--- src/domains.jl
+++ src/domains.jl
@@ -1,4 +1,15 @@
-import DomainSets: Interval, Ball
+import DomainSets: Interval, Ball, infimum, supremum
 
 @deprecate IntervalDomain(a,b) Interval(a,b)
 @deprecate CircleDomain() Ball()
+
+# type piracy on Interval for downstream compatibility to be reverted once upgrade is complete
+function Base.getproperty(domain::Interval, sym::Symbol)
+    if sym === :lower
+        return infimum(domain) # or domain.left also defined by IntervalSets.jl
+    elseif sym === :upper
+        return supremum(domain) # or domain.right
+    else
+        return getfield(domain, sym)
+    end
+end
