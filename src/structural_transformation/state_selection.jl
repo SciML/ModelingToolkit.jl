ac8aa70bf791b2d411d442322ce74cd34541a96a
Yingbo Ma <mayingbo5@gmail.com>
2021-06-06 23:34:45 -0400

    WIP

--- src/structural_transformation/state_selection.jl
+++ src/structural_transformation/state_selection.jl
@@ -39,6 +39,12 @@ function prepare_state_selection!(sys)
         assign[j] = eq_reidx[a]
     end
 
+    @set! s.inv_varassoc = inverse_mapping(s.varassoc)
+    @set! s.inv_eqassoc = inverse_mapping(s.eqassoc)
+    @set! s.inv_assign = inverse_mapping(assign)
+    @set! s.assign = assign
+    @set! s.scc = scc
+
     # Otter and Elmqvist (2017) 4.3.1:
     # All variables $v_{j}$ that appear differentiated and their derivatives
     # with exception of their highest derivatives are potential states, i.e.,
@@ -55,33 +61,101 @@ function prepare_state_selection!(sys)
         s.varmask[j] = !is_potential_state
     end
 
-    e_constraint_set = Vector{Vector{Int}}[]
-    v_constraint_set = Vector{Vector{Int}}[]
+    eq_constraint_set = Vector{Vector{Int}}[]
+    var_constraint_set = Vector{Vector{Int}}[]
 
-    tearing_graph = BipartiteGraph(nsrcs(s.graph), 0, Val(false))
-
-    function check_component(c, eqassoc) # return is_highest_order
-        # Fast paths
-        isempty(c) && return true
-        length(c) == 1 && return eqassoc[c[1]] == 0
-
-        # If there are more than one equation in the component, it must contain
-        # highest order equations only.
-        for eq in c; eqassoc[eq] == 0 && continue
-            error("Internal error: please file a bug report with a full reproducer.")
-        end
-        return true
-    end
+    @unpack graph = s
+    unknow_subgraph = BipartiteGraph(nsrcs(graph), ndsts(graph), Val(false))
+    original_vars = trues(ndsts(graph))
 
     for c in scc
         check_component(c, eqassoc) || break # skip lower order equations
-        # TODO: constraint set
+        eq_constraints, var_constraints = sorted_constraints(c, s)
+        push!(eq_constraint_set, eq_constraints)
+        push!(var_constraint_set, var_constraints)
+
+        var_set = Set(var_constraints)
+        for (i, eqs) in enumerate(eq_constraints), eq in eqs
+            for var in 𝑠neighbors(graph, eq); var in var_set || continue
+                add_edge!(unknow_subgraph, eq, var)
+            end
+
+            if inv_eqassoc[eq] != 0 # differentiated equations
+                for var in 𝑠neighbors(graph, eq)
+                    original_vars[var] = false
+                end
+            end
+        end
     end
 
-    @set! sys.structure.assign = assign
-    @set! sys.structure.inv_assign = inverse_mapping(assign)
-    @set! sys.inv_varassoc = inverse_mapping(s.varassoc)
-    @set! sys.inv_eqassoc = inverse_mapping(s.eqassoc)
-    @set! sys.structure.scc = scc
+    for constraints in Iterators.reverse(zip(eq_constraints, var_constraints))
+        highest_order = length(first(constraints))
+        for (order, (eq_constraint, var_constraint)) in enumerate(zip(constraints...))
+            if length(eq_constraint) == 1 == length(var_constraints)
+                if order < highest_order
+                    # There is only one equation and one unknown in this local
+                    # contraint set, while the equation is lower order. This
+                    # implies that the unknown must be a dummy state.
+                    v = var_constraint[1]
+                    @assert !s.varmask[v]
+                    s.varmask[v] = true
+                else
+
+                end
+            end
+        end
+    end
+
+    @set! sys.structure = s
     return sys
+end
+
+@noinline scc_throw() = error("Internal error: " *
+                              "Strongly connected component invariant is violated!\n" *
+                              "Please file a bug report with full stack trace " *
+                              "and a reproducer.")
+function check_component(c, eqassoc) # return `is_highest_order`
+    # Fast paths
+    isempty(c) && return true
+    length(c) == 1 && return eqassoc[c[1]] == 0
+
+    # If there are more than one equation in the component, it must contain
+    # highest order equations only.
+    for eq in c; eqassoc[eq] == 0 && continue
+        scc_throw()
+    end
+    return true
+end
+
+"""
+$(TYPEDSIGNATURES)
+
+The constraints are sorted in descending order.
+"""
+function sorted_constraints(c::Vector{Int}, s::SystemStructure)
+    @unpack inv_varassoc, inv_eqassoc, inv_assign = s
+    # The set of variables that this compoent solves for
+    unknowns = [inv_assign[eq] for eq in c]
+    eq_constraints = [c]
+    var_constraints = [unknowns]
+    while true
+        lower_eqs = Int[]
+        for eq in eq_constraints[end]
+            lower_eq = inv_eqassoc[eq]
+            lower_eq > 0 && push!(lower_eqs, lower_eq)
+        end
+        isempty(lower_eqs) && break
+
+        push!(eq_constraints, lower_eqs)
+
+        vars = Int[]
+        for var in var_constraints[end]
+            lower_var = inv_varassoc[var]
+            lower_var && push!(vars, lower_var)
+        end
+        push!(var_constraints, vars)
+
+        length(vars) < length(lower_eqs) && scc_throw()
+    end
+    return eq_constraints, var_constraints
 end
