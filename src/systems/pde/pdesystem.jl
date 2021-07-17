13b1c8909d29cfb4517db5251fc2c323b0611bdf
Lucas Morton <23484003+lamorton@users.noreply.github.com>
2021-07-17 13:21:24 -0700

    Force depwarn.

--- src/systems/pde/pdesystem.jl
+++ src/systems/pde/pdesystem.jl
@@ -68,11 +68,11 @@ end
 function Base.getproperty(x::PDESystem, sym::Symbol)
     if sym == :indvars
         return getfield(x, :ivs)
-        depwarn("`sys.indvars` is deprecated, please use `get_ivs(sys)`", :getproperty)
+        depwarn("`sys.indvars` is deprecated, please use `get_ivs(sys)`", :getproperty,force=true)
 
     elseif sym == :depvars
         return getfield(x, :dvs)
-        depwarn("`sys.depvars` is deprecated, please use `get_dvs(sys)`", :getproperty)
+        depwarn("`sys.depvars` is deprecated, please use `get_dvs(sys)`", :getproperty,force=true)
 
     else
         return getfield(x, sym)
