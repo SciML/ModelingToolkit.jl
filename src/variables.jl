18a02d531964245e53c4e3cc892a4c5bdf4b45b5
Sharan Yalburgi <sharanyalburgi@gmail.com>
2021-07-21 19:10:44 +0530

    Fix misspelled "variable" in new and old code

--- src/variables.jl
+++ src/variables.jl
@@ -1,15 +1,15 @@
 struct VariableUnit end
 struct VariableConnectType end
-struct VariabelNoiseType end
-struct VariabelDescriptionType end
-struct VariabelInput end
-struct VariabelOutput end
+struct VariableNoiseType end
+struct VariableDescriptionType end
+struct VariableInput end
+struct VariableOutput end
 Symbolics.option_to_metadata_type(::Val{:unit}) = VariableUnit
 Symbolics.option_to_metadata_type(::Val{:connect}) = VariableConnectType
-Symbolics.option_to_metadata_type(::Val{:noise}) = VariabelNoiseType
-Symbolics.option_to_metadata_type(::Val{:description}) = VariabelDescriptionType
-Symbolics.option_to_metadata_type(::Val{:input}) = VariabelInput
-Symbolics.option_to_metadata_type(::Val{:output}) = VariabelOutput
+Symbolics.option_to_metadata_type(::Val{:noise}) = VariableNoiseType
+Symbolics.option_to_metadata_type(::Val{:description}) = VariableDescriptionType
+Symbolics.option_to_metadata_type(::Val{:input}) = VariableInput
+Symbolics.option_to_metadata_type(::Val{:output}) = VariableOutput
 
 """
 $(SIGNATURES)
