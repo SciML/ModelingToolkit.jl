2983755edc7aee42af2c18ec9adbae53560c5925
Sharan Yalburgi <sharanyalburgi@gmail.com>
2021-08-05 22:58:21 +0530

    Remove 'Symbolics.' prefix for exported functions

--- src/systems/discrete_system/discrete_system.jl
+++ src/systems/discrete_system/discrete_system.jl
@@ -130,8 +130,8 @@ function linearize_eqs(sys, eqs=sys.eqs; return_max_delay=false)
     unique_states = unique(operation.(sys.states))
     max_delay = Dict(v=>0.0 for v in unique_states)
 
-    r = @rule ~t::(t -> t isa Symbolics.Term && any(isequal((t).f), Symbolics.operation.(sys.states)) && is_delay_var(sys.iv, t)) => begin
-        delay = get_delay_val(sys.iv, first(Symbolics.arguments(~t)))
+    r = @rule ~t::(t -> t isa Term && any(isequal((t).f), operation.(sys.states)) && is_delay_var(sys.iv, t)) => begin
+        delay = get_delay_val(sys.iv, first(arguments(~t)))
         if delay > max_delay[operation(~t)]
             max_delay[operation(~t)] = delay
         end
