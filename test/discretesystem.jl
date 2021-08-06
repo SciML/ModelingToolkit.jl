8dbc545f18290956fd625a740ea6db028697ac5e
Sharan Yalburgi <sharanyalburgi@gmail.com>
2021-08-06 14:23:45 +0530

    Fix test bug

--- test/discretesystem.jl
+++ test/discretesystem.jl
@@ -75,7 +75,7 @@ eqs = [
 ]
 
 # System
-sys = DiscreteSystem(eqs,t,[x(t),x(t-1),x(t-2),x(t-3),y(t),y(t-2),z],[])
+sys = DiscreteSystem(eqs,t,[x(t),x(t-1.5),x(t-3),y(t),y(t-2),z],[])
 
 eqs2, max_delay = ModelingToolkit.linearize_eqs(sys; return_max_delay=true)
 
