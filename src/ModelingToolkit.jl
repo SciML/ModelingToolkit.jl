71b97d2fd6f8f6cef63bd2c96cbc5cedda61d442
Fredrik Bagge Carlson <cont-frb@ulund.org>
2021-11-27 07:48:57 +0100

    add export

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -216,6 +216,6 @@ export @named, @nonamespace, @namespace, extend, compose
 export Continuous, Discrete, sampletime, input_timedomain, output_timedomain
 export has_discrete_domain, has_continuous_domain
 export is_discrete_domain, is_continuous_domain, is_hybrid_domain
-export Sample, Hold, Shift
+export Sample, Hold, Shift, SampledTime
 
 end # module
