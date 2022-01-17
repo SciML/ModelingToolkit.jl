af5bbfa125db8a01b13b4f2aa5dc02c5919cbd9f
Documenter.jl <documenter@juliadocs.github.io>
2022-01-17 04:17:12 +0000

    build based on ec5a5f2

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -72,6 +72,41 @@ $(document).ready(function() {
 
 })
 ////////////////////////////////////////////////////////////////////////////////
+require([], function() {
+function addCopyButtonCallbacks() {
+  for (const el of document.getElementsByTagName("pre")) {
+    const button = document.createElement("button");
+    button.classList.add("copy-button", "fas", "fa-copy");
+    el.appendChild(button);
+
+    button.addEventListener("click", function () {
+      navigator.clipboard.writeText(el.innerText).then(
+        function () {
+          button.classList.add("success", "fa-check");
+          button.classList.remove("fa-copy");
+        },
+        function () {
+          button.classList.add("error", "fa-times");
+          button.classList.remove("fa-copy");
+        }
+      );
+
+      setTimeout(function () {
+        button.classList.add("fa-copy");
+        button.classList.remove("success", "fa-check", "fa-times");
+      }, 5000);
+    });
+  }
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", addCopyButtonCallbacks);
+} else {
+  addCopyButtonCallbacks();
+}
+
+})
+////////////////////////////////////////////////////////////////////////////////
 require(['jquery', 'headroom', 'headroom-jquery'], function($, Headroom) {
 
 // Manages the top navigation bar (hides it when the user starts scrolling down on the
