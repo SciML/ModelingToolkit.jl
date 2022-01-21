bb3b47e567e3c38bc6bf009c12a01e2e2084e49b
Documenter.jl <documenter@juliadocs.github.io>
2022-01-21 17:37:50 +0000

    build based on 3f96b01

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -79,22 +79,48 @@ function addCopyButtonCallbacks() {
     button.classList.add("copy-button", "fas", "fa-copy");
     el.appendChild(button);
 
+    const success = function () {
+      button.classList.add("success", "fa-check");
+      button.classList.remove("fa-copy");
+    };
+
+    const failure = function () {
+      button.classList.add("error", "fa-times");
+      button.classList.remove("fa-copy");
+    };
+
     button.addEventListener("click", function () {
-      navigator.clipboard.writeText(el.innerText).then(
-        function () {
-          button.classList.add("success", "fa-check");
-          button.classList.remove("fa-copy");
-        },
-        function () {
-          button.classList.add("error", "fa-times");
-          button.classList.remove("fa-copy");
-        }
-      );
+      copyToClipboard(el.innerText).then(success, failure);
 
       setTimeout(function () {
         button.classList.add("fa-copy");
         button.classList.remove("success", "fa-check", "fa-times");
       }, 5000);
+    });
+  }
+}
+
+function copyToClipboard(text) {
+  // clipboard API is only available in secure contexts
+  if (window.navigator && window.navigator.clipboard) {
+    return window.navigator.clipboard.writeText(text);
+  } else {
+    return new Promise(function (resolve, reject) {
+      try {
+        const el = document.createElement("textarea");
+        el.textContent = text;
+        el.style.position = "fixed";
+        el.style.opacity = 0;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand("copy");
+
+        resolve();
+      } catch (err) {
+        reject(err);
+      } finally {
+        document.body.removeChild(el);
+      }
     });
   }
 }
