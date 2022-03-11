ad2590663e4187af14c2417112c2a1d24d26270e
Oscar Smith <oscardssmith@gmail.com>
2022-03-11 11:14:50 -0500

    add tests for bareiss, fix precompile (#1455)
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- test/structural_transformation/runtests.jl
+++ test/structural_transformation/runtests.jl
@@ -3,3 +3,4 @@ using SafeTestsets
 @safetestset "Utilities" begin include("utils.jl") end
 @safetestset "Index Reduction & SCC" begin include("index_reduction.jl") end
 @safetestset "Tearing" begin include("tearing.jl") end
+@safetestset "Bareiss" begin include("bareiss.jl") end
