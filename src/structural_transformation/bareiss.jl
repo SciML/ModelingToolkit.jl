636313c44e0662c3e874b79b563951f0a976d92f
Oscar Smith <oscardssmith@gmail.com>
2022-03-11 01:07:19 -0600

    Update src/structural_transformation/bareiss.jl
    Co-authored-by: Christopher Rackauckas <accounts@chrisrackauckas.com>

--- src/structural_transformation/bareiss.jl
+++ src/structural_transformation/bareiss.jl
@@ -9,7 +9,7 @@ macro swap(a, b)
 end
 
 # https://github.com/JuliaLang/julia/pull/42678
-if VERSION > v"1.8.0-DEV.762"
+@static if VERSION > v"1.8.0-DEV.762"
     import Base: swaprows!
 else
     function swaprows!(a::AbstractMatrix, i, j)
