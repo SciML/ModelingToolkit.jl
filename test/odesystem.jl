0d83b273edc5450e46b7a2f79efafb7df6421648
Yingbo Ma <mayingbo5@gmail.com>
2022-05-03 12:17:01 -0400

    More tests

--- test/odesystem.jl
+++ test/odesystem.jl
@@ -641,7 +641,7 @@ end
 #issue 1475 (mixed numeric type for parameters)
 let
     @parameters k1 k2
-    @variables t, A(t)
+    @variables t A(t)
     D = Differential(t)
     eqs = [D(A) ~ -k1*k2*A]
     @named sys = ODESystem(eqs,t)
@@ -655,6 +655,11 @@ let
     tspan = (0.0,1.0)
     prob = ODEProblem(sys, u0map, tspan, pmap)
     @test eltype(prob.p) === Float64
+
+    pmap = Pair{Any,Union{Int,Float64}}[k1 => 1, k2 => 1.0]
+    tspan = (0.0,1.0)
+    prob = ODEProblem(sys, u0map, tspan, pmap, use_union=true)
+    @test eltype(prob.p) === Union{Float64,Int}
 end
 
 let
