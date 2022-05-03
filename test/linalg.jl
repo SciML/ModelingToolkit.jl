19d9df3a7c19f00e8691e2f3a330de0e16c0577f
Yingbo Ma <mayingbo5@gmail.com>
2022-05-03 17:54:52 -0400

    Add tests

---
+++ test/linalg.jl
@@ -0,0 +1,17 @@
+using ModelingToolkit
+using Test
+
+A = [
+     0  1   1   2   2  1  1   2  1   2
+     0  1  -1  -3  -2  2  1  -5  0  -5
+     0  1   2   2   1  1  2   1  1   2
+     0  1   1   1   2  1  1   2  2   1
+     0  2   1   2   2  2  2   1  1   1
+     0  1   1   1   2  2  1   1  2   1
+     0  2   1   2   2  1  2   1  1   2
+     0  1   7  17  14  2  1  19  4  23
+     0  1  -1  -3  -2  1  1  -4  0  -5
+     0  1   1   2   2  1  1   2  2   2
+    ]
+N = ModelingToolkit.nullspace(A)
+@test iszero(A * N)
