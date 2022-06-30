33c492af644652aee11e9d091bc41a94f72ae729
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-06-30 10:17:30 +0200

    add tests for linearize

--- test/linearize.jl
+++ test/linearize.jl
@@ -11,7 +11,13 @@ eqs = [u ~ kp * (r - y)
        y ~ x]
 
 @named sys = ODESystem(eqs, t)
-linearize(sys, [r], [y])
+
+lsys, ssys = linearize(sys, [r], [y])
+
+@test lsys.A[] == -2
+@test lsys.B[] == 1
+@test lsys.C[] == 1
+@test lsys.D[] == 0
 
 ##
 ```
@@ -27,7 +33,7 @@ linearize(sys, [r], [y])
 
 function plant(; name)
     @variables x(t) = 1
-    @variables u(t)=0 [input = true] y(t)=0 [output = true]
+    @variables u(t)=0 y(t)=0
     D = Differential(t)
     eqs = [D(x) ~ -x + u
            y ~ x]
@@ -35,7 +41,7 @@ function plant(; name)
 end
 
 function filt_(; name)
-    @variables x(t)=0 y(t)=0 [output = true]
+    @variables x(t)=0 y(t)=0
     @variables u(t)=0 [input = true]
     D = Differential(t)
     eqs = [D(x) ~ -2 * x + u
@@ -44,7 +50,7 @@ function filt_(; name)
 end
 
 function controller(kp; name)
-    @variables y(t)=0 r(t)=0 [input = true] u(t)=0
+    @variables y(t)=0 r(t)=0 u(t)=0
     @parameters kp = kp
     eqs = [
         u ~ kp * (r - y),
@@ -62,23 +68,38 @@ connections = [f.y ~ c.r # filtered reference to controller reference
 
 @named cl = ODESystem(connections, t, systems = [f, c, p])
 
-lin, xs = linearize(cl, cl.f.u, cl.p.x)
+lsys, ssys = linearize(cl, [f.u], [p.x])
+desired_order = [f.x, p.x]
+lsys = ModelingToolkit.reorder_states(lsys, states(ssys), desired_order)
+
+@test lsys.A == [-2 0; 1 -2]
+@test lsys.B == [1; 0;;]
+@test lsys.C == [0 1]
+@test lsys.D[] == 0
 
 ##
 using ModelingToolkitStandardLibrary.Blocks: LimPID
-#using ControlSystems
-k = 400;
-Ti = 0.5;
-Td = 1;
-Nd = 10;
-#s = tf("s")
-#expected_result_r = k*(1 + 1/(s*Ti)) |> ss
-#expected_result_y = k*(1 + 1/(s*Ti) - s*Td / (1 + s*Td/N)) |> ss
+k = 400
+Ti = 0.5
+Td = 1
+Nd = 10
 @named pid = LimPID(; k, Ti, Td, Nd)
-ModelingToolkit.unbound_inputs(pid)
 
 @unpack reference, measurement, ctr_output = pid
-lin = linearize(pid, [reference.u, measurement.u], [ctr_output.u])
-lin, lin_fun = linearize(pid, [reference.u, measurement.u], [ctr_output.u]);
-prob = ODEProblem(lin, [], (0.0, 1.0))
-lin_fun(prob.u0, prob.p, 0.0)
+lsys, ssys = linearize(pid, [reference.u, measurement.u], [ctr_output.u])
+@unpack int, der = pid
+desired_order = [int.x, der.x]
+lsys = ModelingToolkit.reorder_states(lsys, states(ssys), desired_order)
+
+@test lsys.A == [0 0; 0 -10]
+@test lsys.B == [2 -2; 10 -10]
+@test lsys.C == [400 -4000]
+@test lsys.D == [4400 -4400]
+
+# Test with the reverse desired state order as well to verify that similarity transform and reoreder_states really works
+lsys = ModelingToolkit.reorder_states(lsys, states(ssys), reverse(desired_order))
+
+@test lsys.A == [-10 0; 0 0]
+@test lsys.B == [10 -10; 2 -2]
+@test lsys.C == [-4000 400]
+@test lsys.D == [4400 -4400]
