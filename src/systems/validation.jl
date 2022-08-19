a88c1ff9ed16cbd5a87f0125142e4a31a5a199c7
Wiktor Phillips <wsphillips@gmail.com>
2022-08-18 21:45:49 -0400

    format

--- src/systems/validation.jl
+++ src/systems/validation.jl
@@ -132,7 +132,7 @@ end
 
 "Get unit of term, returning nothing & showing warning instead of throwing errors."
 function safe_get_unit(term, info)
-    if term isa Union{Num,Complex{Num},Symbolic} && hasmetadata(term, VariableUnit)
+    if term isa Union{Num, Complex{Num}, Symbolic} && hasmetadata(term, VariableUnit)
         return getmetadata(term, VariableUnit)
     end
     side = nothing
