22035d521fb93a7d569f9b596044222a9d7305bc
Lucas Morton <23484003+lamorton@users.noreply.github.com>
2022-09-19 08:10:32 -0700

    Removed attempt to support array constants.

--- src/constants.jl
+++ src/constants.jl
@@ -1,27 +1,23 @@
 import SymbolicUtils: symtype, term, hasmetadata, issym
 struct MTKConstantCtx end
 
+isconstant(x::Num) = isconstant(unwrap(x))
 function isconstant(x)
     x = unwrap(x)
     x isa Symbolic && getmetadata(x, MTKConstantCtx, false)
 end
-isconstant(x::Num) = isconstant(unwrap(x))
+
 """
     toconst(s::Sym)
 
 Maps the parameter to a constant. The parameter must have a default.
 """
-function toconstant(s)
-    if s isa Symbolics.Arr
-        Symbolics.wrap(toconstant(Symbolics.unwrap(s)))
-    elseif s isa AbstractArray
-        map(toconstant, s)
-    else
-        hasmetadata(s, Symbolics.VariableDefaultValue) ||
-            throw(ArgumentError("Constant `$(s)` must be assigned a default value."))
-        setmetadata(s, MTKConstantCtx, true)
-    end
+function toconstant(s::Sym)
+    hasmetadata(s, Symbolics.VariableDefaultValue) ||
+        throw(ArgumentError("Constant `$(s)` must be assigned a default value."))
+    setmetadata(s, MTKConstantCtx, true)
 end
+
 toconstant(s::Num) = wrap(toconstant(value(s)))
 
 """
