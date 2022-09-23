d65d46b76d7078cf3eb5fc6daf9e4b2a4eb6f9db
xtalax <alex.lemony@gmail.com>
2022-09-23 17:29:30 +0100

    test fix

--- test/odesystem.jl
+++ test/odesystem.jl
@@ -228,10 +228,12 @@ p3 = [k₁ => 0.05,
       k₃ => 1.1e4]
 prob_pmap = remake(prob14; p = p3)
 prob_dpmap = remake(prob14; p = Dict(p3))
-for prob in [prob_pmap, prob_dpmap]
-    @test Set(Num.(parameters(sys)) .=> prob.p) == Set([k₁ => 0.05, k₂ => 2e7, k₃ => 1.1e4])
-    @test Set(Num.(states(sys)) .=> prob.u0) == Set([y₁ => 1, y₂ => 0, y₃ => 0])
-end
+
+@test Set(Num.(parameters(sys)) .=> prob_pmap.p) == Set([k₁ => 0.05, k₂ => 2e7, k₃ => 1.1e4])
+@test Set(Num.(states(sys)) .=> prob_pmap.u0) == Set([y₁ => 1, y₂ => 0, y₃ => 0])
+
+@test solve(prob_pmap) ≈ solve(prob_dpmap) atol = 1e-5
+
 # test kwargs
 prob2 = ODEProblem(sys, u0, tspan, p, jac = true)
 prob3 = ODEProblem(sys, u0, tspan, p, jac = true, sparse = true)
