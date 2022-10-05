5c31d08694c67c7a525b9651fc8b50a96dd67e1b
Yingbo Ma <mayingbo5@gmail.com>
2022-10-05 15:46:54 -0400

    Doc string

--- src/systems/abstractsystem.jl
+++ src/systems/abstractsystem.jl
@@ -163,6 +163,13 @@ independent_variables(sys::AbstractTimeIndependentSystem) = []
 independent_variables(sys::AbstractMultivariateSystem) = getfield(sys, :ivs)
 
 iscomplete(sys::AbstractSystem) = isdefined(sys, :complete) && getfield(sys, :complete)
+
+"""
+$(TYPEDSIGNATURES)
+
+Mark a system as completed. If a system is complete, the system will no longer
+namespace its subsystems or variables, i.e. `isequal(complete(sys).v.i, v.i)`.
+"""
 function complete(sys::AbstractSystem)
     isdefined(sys, :complete) ? (@set! sys.complete = true) : sys
 end
