84a897c7d4aba56a8b5ffec6ffae4f91104aff61
Yingbo Ma <mayingbo5@gmail.com>
2022-10-21 15:13:58 -0400

    Update abstractodesystem.jl

--- src/systems/diffeqs/abstractodesystem.jl
+++ src/systems/diffeqs/abstractodesystem.jl
@@ -556,7 +556,7 @@ function ODEFunctionExpr{iip}(sys::AbstractODESystem, dvs = states(sys),
                           syms = $(Symbol.(states(sys))),
                           indepsym = $(QuoteNode(Symbol(get_iv(sys)))),
                           paramsyms = $(Symbol.(parameters(sys))),
-                          sparsity = $(jacobian_sparsity(sys)))
+                          sparsity = $sparsity ? $(jacobian_sparsity(sys)) : $nothing)
     end
     !linenumbers ? striplines(ex) : ex
 end
