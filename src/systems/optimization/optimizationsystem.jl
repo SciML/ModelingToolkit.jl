011f9b110e12b927d3e7b571784d1d6bbde36a51
Valentin Kaisermayer <valentin.kaisermayer@best-research.eu>
2022-11-04 13:49:11 +0100

    formatting

--- src/systems/optimization/optimizationsystem.jl
+++ src/systems/optimization/optimizationsystem.jl
@@ -214,9 +214,9 @@ parameters with `x[i]` and `p[i]`.
 - `sys`: Reference to the system holding the parameters and states
 - `expand_expr=false`: If `true` the symbolic expression is expanded first.
 """
-function convert_to_expr(eq, sys; expand_expr=false)
+function convert_to_expr(eq, sys; expand_expr = false)
     pairs_arr = expr_map(sys)
-    if expand_expr 
+    if expand_expr
         eq = expand(eq)
     end
     expr = toexpr(eq)
