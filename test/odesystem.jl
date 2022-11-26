02963a5ce94f10d19df99ba9fa38d6eb8e8d6d4a
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-11-26 19:26:19 +0100

    again

--- test/odesystem.jl
+++ test/odesystem.jl
@@ -467,7 +467,7 @@ prob = ODEProblem(sys, [], (0, 1.0))
 sol = solve(prob, Tsit5())
 @test sol[2x[1] + 3x[3] + norm(x)] ≈
       2sol[x[1]] + 3sol[x[3]] + sol[norm(x)]
-@test sol[x + [y, 2y, 3y]] ≈ [sol[x] .+ sol[y]
+@test sol[x .+ [y, 2y, 3y]] ≈ [sol[x] .+ sol[y]
                               sol[x] .+ 2sol[y]
                               sol[x] .+ 3sol[y]]
 
