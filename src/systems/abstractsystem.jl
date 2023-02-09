36a625fd7a082de55f4d7b1b2fdddde349942ef6
Yingbo Ma <mayingbo5@gmail.com>
2023-02-09 10:09:54 -0500

    Rewrap after unwrap in `apply_to_variables`

--- src/systems/abstractsystem.jl
+++ src/systems/abstractsystem.jl
@@ -363,14 +363,15 @@ function Base.setproperty!(sys::AbstractSystem, prop::Symbol, val)
     end
 end
 
-function apply_to_variables(f::F, ex) where {F}
-    ex = value(ex)
+apply_to_variables(f::F, ex) where {F} = _apply_to_variables(f, ex)
+apply_to_variables(f::F, ex::Num) where {F} = wrap(_apply_to_variables(f, unwrap(ex)))
+function _apply_to_variables(f::F, ex) where {F}
     if isvariable(ex)
         return f(ex)
     end
     istree(ex) || return ex
-    similarterm(ex, apply_to_variables(f, operation(ex)),
-                map(Base.Fix1(apply_to_variables, f), arguments(ex)),
+    similarterm(ex, _apply_to_variables(f, operation(ex)),
+                map(Base.Fix1(_apply_to_variables, f), arguments(ex)),
                 metadata = metadata(ex))
 end
 
