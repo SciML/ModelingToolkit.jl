b3d7449e393d9bf78e92b49901af212115e2c5ec
xtalax <alex.lemony@gmail.com>
2023-02-11 16:50:52 +0000

    ps

--- src/systems/pde/pdesystem.jl
+++ src/systems/pde/pdesystem.jl
@@ -112,7 +112,7 @@ struct PDESystem <: ModelingToolkit.AbstractMultivariateSystem
                 analytic_func = map(analytic) do eq
                     args = arguments(eq.lhs)
                     p = ps isa SciMLBase.NullParameters ? [] : map(a -> a.first, ps)
-                    kwargs = :ps ← get(p, ones(length(p)))
+                    kwargs = :ps ← p
                     ex = Func(args, kwargs, eq.rhs) |> toexpr
                     eq.lhs => @RuntimeGeneratedFunction(ex)
                 end
