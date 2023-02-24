0c30bad0b56bb47925601ab2a1a91bf86b77ade2
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-02-24 12:28:44 +0100

    use `get_u0_p` for more system types

--- src/systems/nonlinear/nonlinearsystem.jl
+++ src/systems/nonlinear/nonlinearsystem.jl
@@ -325,12 +325,7 @@ function process_NonlinearProblem(constructor, sys::NonlinearSystem, u0map, para
     dvs = states(sys)
     ps = parameters(sys)
 
-    defs = defaults(sys)
-    defs = mergedefaults(defs, parammap, ps)
-    defs = mergedefaults(defs, u0map, dvs)
-
-    u0 = varmap_to_vars(u0map, dvs; defaults = defs, tofloat = true)
-    p = varmap_to_vars(parammap, ps; defaults = defs, tofloat = !use_union, use_union)
+    u0, p, defs = get_u0_p(sys, u0map, parammap; tofloat, use_union)
 
     check_eqs_u0(eqs, dvs, u0; kwargs...)
 
