057baf9953aa57c06f67c3145f5dcd216a7ca209
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2023-06-22 02:40:42 +0530

    test: add tests to verify heirarchal kwargs and metadata and default values of different parameter definitions

--- test/model_parsing.jl
+++ test/model_parsing.jl
@@ -1,5 +1,5 @@
 using ModelingToolkit, Test
-using ModelingToolkit: get_gui_metadata
+using ModelingToolkit: get_gui_metadata, VariableDescription, getdefault
 using URIs: URI
 
 ENV["MTK_ICONS_DIR"] = "$(@__DIR__)/icons"
@@ -10,12 +10,12 @@ end
 @connector RealOutput begin
     u(t), [output = true]
 end
-@model Constant begin
+@model Constant(; k = 1) begin
     @components begin
         output = RealOutput()
     end
     @parameters begin
-        k, [description = "Constant output value of block"]
+        k = k, [description = "Constant output value of block"]
     end
     @equations begin
         output.u ~ k
@@ -61,10 +61,10 @@ end
 end
 
 resistor_log = "$(@__DIR__)/logo/resistor.svg"
-@model Resistor begin
+@model Resistor(; R = 1) begin
     @extend v, i = oneport = OnePort()
     @parameters begin
-        R = 1
+        R = R
     end
     @icon begin
         """<?xml version="1.0" encoding="UTF-8"?>
@@ -87,10 +87,10 @@ l15 0" stroke="black" stroke-width="1" stroke-linejoin="bevel" fill="none"></pat
     end
 end
 
-@model Capacitor begin
+@model Capacitor(; C = 1) begin
     @extend v, i = oneport = OnePort()
     @parameters begin
-        C = 1
+        C = C
     end
     @icon "https://upload.wikimedia.org/wikipedia/commons/7/78/Capacitor_symbol.svg"
     @equations begin
@@ -110,8 +110,8 @@ end
 
 @model RC begin
     @components begin
-        resistor = Resistor()
-        capacitor = Capacitor()
+        resistor = Resistor(; R)
+        capacitor = Capacitor(; C = 10)
         source = Voltage()
         constant = Constant()
         ground = Ground()
@@ -123,7 +123,11 @@ end
         connect(capacitor.n, source.n, ground.g)
     end
 end
-@named rc = RC()
+
+@named rc = RC(; resistor.R = 20)
+@test getdefault(rc.resistor.R) == 20
+@test getdefault(rc.capacitor.C) == 10
+@test getdefault(rc.constant.k) == 1
 
 @test get_gui_metadata(rc.resistor).layout == Resistor.structure[:icon] ==
       read(joinpath(ENV["MTK_ICONS_DIR"], "resistor.svg"), String)
@@ -137,3 +141,34 @@ end
       URI("file:///" * abspath(ENV["MTK_ICONS_DIR"], "pin.png"))
 
 @test length(equations(structural_simplify(rc))) == 1
+
+@model MockModel(; cval, gval, jval = 6) begin
+    @parameters begin
+        a
+        b(t)
+        c(t) = cval
+        d = 2
+        e, [description = "e"]
+        f = 3, [description = "f"]
+        g = gval, [description = "g"]
+        h(t), [description = "h(t)"]
+        i(t) = 5, [description = "i(t)"]
+        j(t) = jval, [description = "j(t)"]
+    end
+end
+
+@named model = MockModel(cval = 1, gval = 4)
+
+@test hasmetadata(model.e, VariableDescription)
+@test hasmetadata(model.f, VariableDescription)
+@test hasmetadata(model.g, VariableDescription)
+@test hasmetadata(model.h, VariableDescription)
+@test hasmetadata(model.i, VariableDescription)
+@test hasmetadata(model.j, VariableDescription)
+
+@test getdefault(model.c) == 1
+@test getdefault(model.d) == 2
+@test getdefault(model.f) == 3
+@test getdefault(model.g) == 4
+@test getdefault(model.i) == 5
+@test getdefault(model.j) == 6
