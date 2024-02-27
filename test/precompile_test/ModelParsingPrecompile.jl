fd8fd7fb8b1bc38ce024c36f9254e026ffcbee19
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2024-02-27 10:52:59 +0530

    test: constants in the mtkmodel
    Adds top-level and the closed module  tests.

--- test/precompile_test/ModelParsingPrecompile.jl
+++ test/precompile_test/ModelParsingPrecompile.jl
@@ -3,8 +3,11 @@ module ModelParsingPrecompile
 using ModelingToolkit, Unitful
 
 @mtkmodel ModelWithComponentArray begin
+    @constants begin
+        k = 1, [description = "Default val of R"]
+    end
     @parameters begin
-        R(t)[1:3] = 1, [description = "Parameter array", unit = u"Ω"]
+        r(t)[1:3] = k, [description = "Parameter array", unit = u"Ω"]
     end
 end
 
