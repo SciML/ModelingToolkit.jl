411efc7fcde4a99ee7f8db98f7ed22df726df9b9
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-03-01 04:52:33 -0600

    weird format

--- test/initializationsystem.jl
+++ test/initializationsystem.jl
@@ -428,4 +428,5 @@ prob = ODEProblem(simpsys, [z => 1.0, y => 1.0], tspan, guesses = [x => 2.0])
 sol = solve(prob, Tsit5())
 @test sol[1] == [0.0, 1.0]
 
-@test_broken @test_warn "Initialization system is underdetermined. 1 equations for 3 unknowns. Initialization will default to using least squares. To suppress this warning pass warn_initialize_determined = false." prob = ODEProblem(simpsys, [], tspan, guesses = [x => 2.0])
+@test_broken @test_warn "Initialization system is underdetermined. 1 equations for 3 unknowns. Initialization will default to using least squares. To suppress this warning pass warn_initialize_determined = false." prob=ODEProblem(
+    simpsys, [], tspan, guesses = [x => 2.0])
