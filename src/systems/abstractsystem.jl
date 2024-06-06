1f41c611d8ffc0f53a360aeae4b434c098acd36f
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-06-05 23:54:22 -0400

    Update src/systems/abstractsystem.jl

--- src/systems/abstractsystem.jl
+++ src/systems/abstractsystem.jl
@@ -2565,9 +2565,9 @@ is_diff_equation(eq2) # false
 """
 function is_diff_equation(eq)
     (eq isa Equation) || (return false)
-    isdefined(eq, :lhs) && SymbolicUtils.hasnode(is_derivative, wrap(eq.lhs)) &&
+    isdefined(eq, :lhs) && hasnode(is_derivative, wrap(eq.lhs)) &&
         (return true)
-    isdefined(eq, :rhs) && SymbolicUtils.hasnode(is_derivative, wrap(eq.rhs)) &&
+    isdefined(eq, :rhs) && hasnode(is_derivative, wrap(eq.rhs)) &&
         (return true)
     return false
 end
