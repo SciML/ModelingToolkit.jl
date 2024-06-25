f1344b2c58987791c298c7a2510b9a2359aef5c4
Aayush Sabharwal <aayush.sabharwal@juliahub.com>
2024-06-25 11:51:48 +0530

    test: test implementation of SII/SciMLBase discrete saving interface

--- test/mtkparameters.jl
+++ test/mtkparameters.jl
@@ -307,3 +307,29 @@ end
     newoprob = remake(oprob_scal_scal; p = ps_vec)
     @test newoprob.ps[k] == [2.0, 3.0, 4.0, 5.0]
 end
+
+# Parameter timeseries
+ps = MTKParameters(([1.0, 1.0],), SizedArray{2}([([0.0, 0.0],), ([0.0, 0.0],)]), (), (), (), nothing, nothing)
+with_updated_parameter_timeseries_values(
+    ps, 1 => ModelingToolkit.NestedGetIndex(([5.0, 10.0],)))
+@test ps.discrete[1][1] == [5.0, 10.0]
+with_updated_parameter_timeseries_values(
+    ps, 1 => ModelingToolkit.NestedGetIndex(([3.0, 30.0],)),
+    2 => ModelingToolkit.NestedGetIndex(([4.0, 40.0],)))
+@test ps.discrete[1][1] == [3.0, 30.0]
+@test ps.discrete[2][1] == [4.0, 40.0]
+@test SciMLBase.get_saveable_values(ps, 1).x == ps.discrete[1]
+
+# With multiple types and clocks
+ps = MTKParameters((), SizedVector{2}([([1.0, 2.0, 3.0], [false]), ([4.0, 5.0, 6.0], Bool[])]), (), (), (), nothing, nothing)
+@test SciMLBase.get_saveable_values(ps, 1).x isa Tuple{Vector{Float64}, Vector{Bool}}
+tsidx1 = 1
+tsidx2 = 2
+@test length(ps.discrete[tsidx1][1]) == 3
+@test length(ps.discrete[tsidx1][2]) == 1
+@test length(ps.discrete[tsidx2][1]) == 3
+@test length(ps.discrete[tsidx2][2]) == 0
+with_updated_parameter_timeseries_values(
+    ps, tsidx1 => ModelingToolkit.NestedGetIndex(([10.0, 11.0, 12.0], [false])))
+@test ps.discrete[tsidx1][1] == [10.0, 11.0, 12.0]
+@test ps.discrete[tsidx1][2][] == false
