8a0415b4e01132db83f7a3025892ee81ff3078a6
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2024-08-21 10:42:41 +0530

    refactor: format

--- ext/MTKChainRulesCoreExt.jl
+++ ext/MTKChainRulesCoreExt.jl
@@ -6,7 +6,8 @@ import ChainRulesCore: NoTangent
 
 function ChainRulesCore.rrule(::Type{MTK.MTKParameters}, tunables, args...)
     function mtp_pullback(dt)
-        (NoTangent(), dt.tunable[1:length(tunables)], ntuple(_ -> NoTangent(), length(args))...)
+        (NoTangent(), dt.tunable[1:length(tunables)],
+            ntuple(_ -> NoTangent(), length(args))...)
     end
     MTK.MTKParameters(tunables, args...), mtp_pullback
 end
