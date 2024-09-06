9ff53fdf53e6c291bbe46a4620b07d688d6c166d
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-09-06 09:18:17 -0400

    Update src/systems/diffeqs/abstractodesystem.jl

--- src/systems/diffeqs/abstractodesystem.jl
+++ src/systems/diffeqs/abstractodesystem.jl
@@ -812,7 +812,6 @@ function process_DEProblem(constructor, sys::AbstractODESystem, u0map, parammap;
     # ModelingToolkit.get_tearing_state(sys) !== nothing => Requires structural_simplify first
     if sys isa ODESystem && build_initializeprob &&
        (((implicit_dae || !isempty(missingvars) || !isempty(setobserved)) &&
-         all(isequal(Continuous()), ci.var_domain) &&
          ModelingToolkit.get_tearing_state(sys) !== nothing) ||
         !isempty(initialization_equations(sys))) && t !== nothing
         if eltype(u0map) <: Number
