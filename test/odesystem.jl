7579312a9b505df442ecce146c2a169a725d7931
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2024-09-19 16:07:48 +0530

    test: fix odesystem callable parameter tests

--- test/odesystem.jl
+++ test/odesystem.jl
@@ -137,7 +137,7 @@ eqs = [D(x) ~ σ(t - 1) * (y - x),
     D(y) ~ x * (ρ - z) - y,
     D(z) ~ x * y - β * z * κ]
 @named de = ODESystem(eqs, t)
-test_diffeq_inference("single internal iv-varying", de, t, (x, y, z), (σ(t - 1), ρ, β))
+test_diffeq_inference("single internal iv-varying", de, t, (x, y, z), (σ, ρ, β))
 f = eval(generate_function(de, [x, y, z], [σ, ρ, β])[2])
 du = [0.0, 0.0, 0.0]
 f(du, [1.0, 2.0, 3.0], [x -> x + 7, 2, 3], 5.0)
@@ -145,7 +145,7 @@ f(du, [1.0, 2.0, 3.0], [x -> x + 7, 2, 3], 5.0)
 
 eqs = [D(x) ~ x + 10σ(t - 1) + 100σ(t - 2) + 1000σ(t^2)]
 @named de = ODESystem(eqs, t)
-test_diffeq_inference("many internal iv-varying", de, t, (x,), (σ(t - 2), σ(t^2), σ(t - 1)))
+test_diffeq_inference("many internal iv-varying", de, t, (x,), (σ,))
 f = eval(generate_function(de, [x], [σ])[2])
 du = [0.0]
 f(du, [1.0], [t -> t + 2], 5.0)
