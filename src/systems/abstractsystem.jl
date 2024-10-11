5d7d97daf58ca4d7beeb3ba496e4c492b9a6f8b4
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-10-11 08:05:40 -0400

    Update abstractsystem.jl

--- src/systems/abstractsystem.jl
+++ src/systems/abstractsystem.jl
@@ -913,7 +913,12 @@ end
 """
 $(TYPEDSIGNATURES)
 
-Mark a system as completed. If a system is complete, the system will no longer
+Mark a system as completed. A completed system is a system which is done being
+defined/modified and is ready for structural analysis or other transformations.
+This allows for analyses and optimizations to be performed which require knowing
+the global structure of the system.
+        
+One property to note is that if a system is complete, the system will no longer
 namespace its subsystems or variables, i.e. `isequal(complete(sys).v.i, v.i)`.
 """
 function complete(sys::AbstractSystem; split = true)
