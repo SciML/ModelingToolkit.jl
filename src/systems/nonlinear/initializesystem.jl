ee2c3dd7512eec4b1c1717fe0b735992e47c3678
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2024-12-01 13:07:33 +0530

    feat: support arbitrary systems in `generate_initializesystem`

--- src/systems/nonlinear/initializesystem.jl
+++ src/systems/nonlinear/initializesystem.jl
@@ -3,7 +3,7 @@ $(TYPEDSIGNATURES)
 
 Generate `NonlinearSystem` which initializes an ODE problem from specified initial conditions of an `ODESystem`.
 """
-function generate_initializesystem(sys::ODESystem;
+function generate_initializesystem(sys::AbstractSystem;
         u0map = Dict(),
         pmap = Dict(),
         initialization_eqs = [],
@@ -12,53 +12,60 @@ function generate_initializesystem(sys::ODESystem;
         algebraic_only = false,
         check_units = true, check_defguess = false,
         name = nameof(sys), extra_metadata = (;), kwargs...)
-    trueobs, eqs = unhack_observed(observed(sys), equations(sys))
+    eqs = equations(sys)
+    if sys isa JumpSystem
+        eqs = eqs.x[4]
+    end
+    trueobs, eqs = unhack_observed(observed(sys), eqs)
     vars = unique([unknowns(sys); getfield.(trueobs, :lhs)])
     vars_set = Set(vars) # for efficient in-lookup
 
-    idxs_diff = isdiffeq.(eqs)
-    idxs_alge = .!idxs_diff
-
-    # prepare map for dummy derivative substitution
-    eqs_diff = eqs[idxs_diff]
-    D = Differential(get_iv(sys))
-    diffmap = merge(
-        Dict(eq.lhs => eq.rhs for eq in eqs_diff),
-        Dict(D(eq.lhs) => D(eq.rhs) for eq in trueobs)
-    )
-
-    # 1) process dummy derivatives and u0map into initialization system
-    eqs_ics = eqs[idxs_alge] # start equation list with algebraic equations
+    eqs_ics = Equation[]
     defs = copy(defaults(sys)) # copy so we don't modify sys.defaults
     additional_guesses = anydict(guesses)
     guesses = merge(get_guesses(sys), additional_guesses)
-    schedule = getfield(sys, :schedule)
-    if !isnothing(schedule)
-        for x in filter(x -> !isnothing(x[1]), schedule.dummy_sub)
-            # set dummy derivatives to default_dd_guess unless specified
-            push!(defs, x[1] => get(guesses, x[1], default_dd_guess))
-        end
-        function process_u0map_with_dummysubs(y, x)
-            y = get(schedule.dummy_sub, y, y)
-            y = fixpoint_sub(y, diffmap)
-            if y ∈ vars_set
-                # variables specified in u0 overrides defaults
-                push!(defs, y => x)
-            elseif y isa Symbolics.Arr
-                # TODO: don't scalarize arrays
-                merge!(defs, Dict(scalarize(y .=> x)))
-            elseif y isa Symbolics.BasicSymbolic
-                # y is a derivative expression expanded; add it to the initialization equations
-                push!(eqs_ics, y ~ x)
-            else
-                error("Initialization expression $y is currently not supported. If its a higher order derivative expression, then only the dummy derivative expressions are supported.")
+
+    if has_iv(sys)
+        idxs_diff = isdiffeq.(eqs)
+        idxs_alge = .!idxs_diff
+
+        # prepare map for dummy derivative substitution
+        eqs_diff = eqs[idxs_diff]
+        D = Differential(get_iv(sys))
+        diffmap = merge(
+            Dict(eq.lhs => eq.rhs for eq in eqs_diff),
+            Dict(D(eq.lhs) => D(eq.rhs) for eq in trueobs)
+        )
+
+        # 1) process dummy derivatives and u0map into initialization system
+        append!(eqs_ics, eqs[idxs_alge]) # start equation list with algebraic equations
+        if has_schedule(sys) && (schedule = get_schedule(sys); !isnothing(schedule))
+            for x in filter(x -> !isnothing(x[1]), schedule.dummy_sub)
+                # set dummy derivatives to default_dd_guess unless specified
+                push!(defs, x[1] => get(guesses, x[1], default_dd_guess))
             end
-        end
-        for (y, x) in u0map
-            if Symbolics.isarraysymbolic(y)
-                process_u0map_with_dummysubs.(collect(y), collect(x))
-            else
-                process_u0map_with_dummysubs(y, x)
+            function process_u0map_with_dummysubs(y, x)
+                y = get(schedule.dummy_sub, y, y)
+                y = fixpoint_sub(y, diffmap)
+                if y ∈ vars_set
+                    # variables specified in u0 overrides defaults
+                    push!(defs, y => x)
+                elseif y isa Symbolics.Arr
+                    # TODO: don't scalarize arrays
+                    merge!(defs, Dict(scalarize(y .=> x)))
+                elseif y isa Symbolics.BasicSymbolic
+                    # y is a derivative expression expanded; add it to the initialization equations
+                    push!(eqs_ics, y ~ x)
+                else
+                    error("Initialization expression $y is currently not supported. If its a higher order derivative expression, then only the dummy derivative expressions are supported.")
+                end
+            end
+            for (y, x) in u0map
+                if Symbolics.isarraysymbolic(y)
+                    process_u0map_with_dummysubs.(collect(y), collect(x))
+                else
+                    process_u0map_with_dummysubs(y, x)
+                end
             end
         end
     end
