cab4506c9d5da1a297915081a92e99b11af2cce7
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-01-24 12:55:17 +0530

    fix: fix `ReconstructInitializeprob` overriding guesses

--- src/systems/nonlinear/initializesystem.jl
+++ src/systems/nonlinear/initializesystem.jl
@@ -333,10 +333,9 @@ end
 
 function ReconstructInitializeprob(
         srcsys::AbstractSystem, dstsys::AbstractSystem; remap = Dict())
-    syms = [unknowns(dstsys);
-            reduce(vcat, reorder_parameters(dstsys, parameters(dstsys)); init = [])]
+    syms = reduce(vcat, reorder_parameters(dstsys, parameters(dstsys)); init = [])
     getter = getu(srcsys, map(x -> get(remap, x, x), syms))
-    setter = setsym_oop(dstsys, syms)
+    setter = setp_oop(dstsys, syms)
     return ReconstructInitializeprob(getter, setter)
 end
 
