cecb15c210b2c5de34ef8f68e6737c76d944cf70
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-02-10 18:46:05 +0530

    feat: export `Initial`

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -278,7 +278,7 @@ export toexpr, get_variables
 export simplify, substitute
 export build_function
 export modelingtoolkitize
-export generate_initializesystem
+export generate_initializesystem, Initial
 
 export alg_equations, diff_equations, has_alg_equations, has_diff_equations
 export get_alg_eqs, get_diff_eqs, has_alg_eqs, has_diff_eqs
