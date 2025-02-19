b257e20cd13822577b389cfedbb0b774b87ca355
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-02-19 17:48:23 +0530

    refactor: don't do CSE hack for operators
    Prevents annoying equations with `Initial` parameters

--- src/structural_transformation/symbolics_tearing.jl
+++ src/structural_transformation/symbolics_tearing.jl
@@ -647,6 +647,7 @@ function cse_and_array_hacks(sys, obs, subeqs, unknowns, neweqs; cse = true, arr
         # HACK 1
         if cse && is_getindexed_array(rhs)
             rhs_arr = arguments(rhs)[1]
+            iscall(rhs_arr) && operation(rhs_arr) isa Symbolics.Operator && continue
             if !haskey(rhs_to_tempvar, rhs_arr)
                 tempvar = gensym(Symbol(lhs))
                 N = length(rhs_arr)
