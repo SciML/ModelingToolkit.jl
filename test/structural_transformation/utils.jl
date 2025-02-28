506522c7f3b8ca2fafe4d205b70542301f26b26d
vyudu <vincent.duyuan@gmail.com>
2025-02-28 11:01:27 -0500

    fix missing var

--- test/structural_transformation/utils.jl
+++ test/structural_transformation/utils.jl
@@ -5,6 +5,7 @@ using SparseArrays
 using UnPack
 using ModelingToolkit: t_nounits as t, D_nounits as D, default_toterm
 using Symbolics: unwrap
+const ST = StructuralTransformations
 
 # Define some variables
 @parameters L g
