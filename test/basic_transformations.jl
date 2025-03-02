c15580c7c62880c96a333735d4821261a11c45b1
Herman Sletmoen <hermansletmoen@gmail.com>
2025-03-02 12:12:47 +0100

    Specify new independent variable as a dependent variable in the old system

--- test/basic_transformations.jl
+++ test/basic_transformations.jl
@@ -26,7 +26,6 @@ D = Differential(t)
 end
 
 @testset "Change independent variable" begin
-    @independent_variables t
     @variables x(t) y(t) z(t) s(t)
     eqs = [
         D(x) ~ y
@@ -34,26 +33,22 @@ end
         z ~ x + D(y)
         D(s) ~ 1 / (2*s)
     ]
-    @named sys1 = ODESystem(eqs, t)
-    sys1 = complete(sys1)
+    M1 = ODESystem(eqs, t; name = :M) |> complete
+    M2 = ModelingToolkit.change_independent_variable(M1, M1.s)
 
-    @independent_variables s
-    sys2 = ModelingToolkit.change_independent_variable(sys1, s)
-
-    sys1 = structural_simplify(sys1; allow_symbolic = true)
-    sys2 = structural_simplify(sys2; allow_symbolic = true)
-    prob1 = ODEProblem(sys1, [sys1.x => 1.0, sys1.y => 1.0, Differential(t)(sys1.y) => 0.0, sys1.s => 1.0], (1.0, 4.0))
-    prob2 = ODEProblem(sys2, [sys2.x => 1.0, sys2.y => 1.0, Differential(s)(sys2.y) => 0.0], (1.0, 2.0))
+    M1 = structural_simplify(M1; allow_symbolic = true)
+    M2 = structural_simplify(M2; allow_symbolic = true)
+    prob1 = ODEProblem(M1, [M1.x => 1.0, M1.y => 1.0, Differential(M1.t)(M1.y) => 0.0, M1.s => 1.0], (1.0, 4.0))
+    prob2 = ODEProblem(M2, [M2.x => 1.0, M2.y => 1.0, Differential(M2.s)(M2.y) => 0.0], (1.0, 2.0))
     sol1 = solve(prob1, Tsit5(); reltol = 1e-10, abstol = 1e-10)
     sol2 = solve(prob2, Tsit5(); reltol = 1e-10, abstol = 1e-10)
     ts = range(0.0, 1.0, length = 50)
     ss = .√(ts)
-    @test all(isapprox.(sol1(ts, idxs=sys1.x), sol2(ss, idxs=sys2.x); atol = 1e-7)) &&
-          all(isapprox.(sol1(ts, idxs=sys1.y), sol2(ss, idxs=sys2.y); atol = 1e-7))
+    @test all(isapprox.(sol1(ts, idxs=M1.x), sol2(ss, idxs=M2.x); atol = 1e-7)) &&
+          all(isapprox.(sol1(ts, idxs=M1.y), sol2(ss, idxs=M2.y); atol = 1e-7))
 end
 
 @testset "Change independent variable (Friedmann equation)" begin
-    @independent_variables t
     D = Differential(t)
     @variables a(t) ρr(t) ρm(t) ρΛ(t) ρ(t) P(t) ϕ(t)
     @parameters Ωr0 Ωm0 ΩΛ0
@@ -68,8 +63,7 @@ end
     @named M1 = ODESystem(eqs, t)
     M1 = complete(M1)
 
-    @independent_variables a
-    M2 = ModelingToolkit.change_independent_variable(M1, a)
+    M2 = ModelingToolkit.change_independent_variable(M1, M1.a)
     M2 = structural_simplify(M2; allow_symbolic = true)
     @test length(unknowns(M2)) == 2
 end
