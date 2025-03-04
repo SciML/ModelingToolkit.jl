d6b3f27b20683c063c6d003ef2ed2db68fb732dc
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-03-04 14:25:32 +0530

    test: remove unnecessary wrapping of parameter vector

--- test/input_output_handling.jl
+++ test/input_output_handling.jl
@@ -145,9 +145,9 @@ if VERSION >= v"1.8" # :opaque_closure not supported before
             inputs = [torque.tau.u])
         x = randn(size(A, 1))
         u = randn(size(B, 2))
-        p = (getindex.(
+        p = getindex.(
             Ref(ModelingToolkit.defaults_and_guesses(ssys)),
-            parameters(ssys)),)
+            parameters(ssys))
         y1 = obsf(x, u, p, 0)
         y2 = C * x + D * u
         @test y1[] ≈ y2[]
