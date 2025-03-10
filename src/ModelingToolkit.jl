db5b8289e4c94b801812809a88ba8e887095fbe7
vyudu <vincent.duyuan@gmail.com>
2025-03-10 16:23:40 -0400

    use Pre in the affect definition

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -299,6 +299,7 @@ export initialization_equations, guesses, defaults, parameter_dependencies, hier
 export structural_simplify, expand_connections, linearize, linearization_function,
        LinearizationProblem
 export solve
+export Pre
 
 export calculate_jacobian, generate_jacobian, generate_function, generate_custom_function
 export calculate_control_jacobian, generate_control_jacobian
