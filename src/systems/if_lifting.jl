9bac25fa252d00b6910045a2f2e592493bc489e9
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-03-17 06:46:10 -0100

    Update if_lifting.jl

--- src/systems/if_lifting.jl
+++ src/systems/if_lifting.jl
@@ -85,7 +85,7 @@ function (cw::CondRewriter)(expr, dep)
         return (expr, true, true)
         # other singleton symbolic variables
     elseif !iscall(expr)
-        @warn "Automatic conversion of if statments to events requires use of a limited conditional grammar; see the documentation. Skipping due to $expr"
+        @warn "Automatic conversion of if statements to events requires use of a limited conditional grammar; see the documentation. Skipping due to $expr"
         return (expr, true, true) # error case => conservative assumption is that both true and false have to be evaluated
     elseif operation(expr) == Base.:(|) # OR of two conditions
         a, b = arguments(expr)
@@ -405,7 +405,7 @@ const CONDITION_SIMPLIFIER = Rewriters.Fixpoint(Rewriters.Postwalk(Rewriters.Cha
                                                                                     (@rule ifelse(false, (~x), (~y)) => (~y))])))
 
 """
-If lifting converts (nested) if statements into a series of continous events + a logically equivalent if statement + parameters.
+If lifting converts (nested) if statements into a series of continuous events + a logically equivalent if statement + parameters.
 
 Lifting proceeds through the following process:
 * rewrite comparisons to be of the form eqn [op] 0; subtract the RHS from the LHS 
