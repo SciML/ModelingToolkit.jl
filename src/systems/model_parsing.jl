57ca008b41b727534b64f341232ae321ba998f19
vyudu <vincent.duyuan@gmail.com>
2025-03-24 19:28:21 -0400

    fix: use is_diff_equation instead of isdiffeq when finding algeeqs

--- src/systems/model_parsing.jl
+++ src/systems/model_parsing.jl
@@ -1115,7 +1115,6 @@ function parse_equations!(exprs, eqs, dict, body)
 end
 
 function parse_continuous_events!(c_evts, dict, body)
-    @show body
     dict[:continuous_events] = []
     Base.remove_linenums!(body)
     for arg in body.args
@@ -1125,7 +1124,6 @@ function parse_continuous_events!(c_evts, dict, body)
 end
 
 function parse_discrete_events!(d_evts, dict, body)
-    @show body
     dict[:discrete_events] = []
     Base.remove_linenums!(body)
     for arg in body.args
