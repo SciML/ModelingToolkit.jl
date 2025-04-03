89d2c0e9f3e1dea89a75ae85c1a9a79266ba4a1f
vyudu <vincent.duyuan@gmail.com>
2025-04-03 18:25:31 -0400

    up

--- ext/MTKJuMPExt.jl
+++ ext/MTKJuMPExt.jl
@@ -4,16 +4,35 @@ using JuMP, InfiniteOpt
 Convert an ODESystem with constraints to a JuMPProblem for optimal control solving.
 """
 function JuMPProblem(sys::ODESystem, u0, tspan, p; dt = error("dt must be provided for JuMPProblem."))
-    steps = tspan[1]:dt:tspan[2]
-    nsteps = length(steps)
-    cost = sys.cost
-    coalesce = sys.coalesce
+    ts = tspan[1]
+    te = tspan[2]
+    steps = ts:dt:te
+    costs = get_costs(sys)
+    consolidate = get_consolidate(sys)
+    ctrls = get_ctrls(sys)
+    states = unknowns(sys)
+    constraints = get_constraints(get_constraintsystem(sys))
 
-    @variables
-    @variables
-    @variables
+    model = Model()
+
+    @infinite_parameter(model, t in [tspan[1],tspan[2]], num_supports = length(steps), derivative_method = OrthogonalCollocation(2))
+    @variables(model, U[1:length(states)], Infinite(t), start = ts)
+    @variables(model, V[1:length(ctrls)], Infinite(t), start = ts)
+    @variables(model, K)
+
+    jcost = generate_jump_cost_function(sys)
+    @objective
+
+    constraints = generate_jump_constraints(constraints)
+    @constraints
 end
 
-function symbolic_to_jump_constraint(cons::Union{Num, BasicSymbolic})
+function generate_jump_cost_function(costs, tsteps)
+end
+
+function generate_jump_constraints(constraints, jump_vars, jump_ps)
+end
+
+function t_to_tstep()
     
 end
