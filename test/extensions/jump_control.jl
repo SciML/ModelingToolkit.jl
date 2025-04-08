f2e3ffde570043361f91a3893ebc45c543a22a24
vyudu <vincent.duyuan@gmail.com>
2025-04-08 14:50:08 -0400

    fix: use dt in the constraints

--- test/extensions/jump_control.jl
+++ test/extensions/jump_control.jl
@@ -2,7 +2,8 @@ using ModelingToolkit
 using JuMP, InfiniteOpt
 using DiffEqDevTools, DiffEqBase
 using SimpleDiffEq
-using HiGHS
+using OrdinaryDiffEqSDIRK
+using Ipopt 
 const M = ModelingToolkit
 
 @testset "ODE Solution, no cost" begin
@@ -22,36 +23,33 @@ const M = ModelingToolkit
     # Test explicit method.
     jprob = JuMPControlProblem(sys, u0map, tspan, parammap, dt = 0.01)
     @test num_constraints(jprob.model) == 2 # initials
-    jsol = solve(jprob, Ipopt.Optimizer, :Tsitouras5)
+    jsol = solve(jprob, Ipopt.Optimizer, :RK4)
     oprob = ODEProblem(sys, u0map, tspan, parammap)
-    osol = solve(oprob, SimpleTsit5(), adaptive = false)
+    osol = solve(oprob, SimpleRK4(), dt = 0.01)
     @test jsol.sol.u ≈ osol.u
 
     # Implicit method.
-    jsol2 = solve(prob, Ipopt.Optimizer, :RK4)
-    osol2 = solve(oprob, SimpleRK4(), adaptive = false)
-    @test jsol2.sol.u ≈ osol2.u
+    jsol2 = solve(jprob, Ipopt.Optimizer, :ImplicitEuler)
+    osol2 = solve(oprob, ImplicitEuler(), dt = 0.01, adaptive = false)
+    @test ≈(jsol2.sol.u, osol2.u, rtol = 0.001)
 
     # With a constraint
-    @parameters α=1.5 β=1.0 γ=3.0 δ=1.0
-    @variables x(..) y(..)
-
-    eqs = [D(x(t)) ~ α * x(t) - β * x(t) * y(t),
-        D(y(t)) ~ -γ * y(t) + δ * x(t) * y(t)]
-
-    u0map = []
-    tspan = (0.0, 1.0)
+    u0map = Pair[]
     guess = [x(t) => 4.0, y(t) => 2.0]
     constr = [x(0.6) ~ 3.5, x(0.3) ~ 7.0]
     @mtkbuild lksys = ODESystem(eqs, t; constraints = constr)
 
     jprob = JuMPControlProblem(sys, u0map, tspan, parammap; guesses, dt = 0.01)
     @test num_constraints(jprob.model) == 2 == num_variables(jprob.model) == 2
-    jsol = solve(prob, HiGHS.Optimizer, :Tsitouras5)
+    jsol = solve(prob, Ipopt.Optimizer, :Tsitouras5)
     sol = jsol.sol
     @test sol(0.6)[1] ≈ 3.5
     @test sol(0.3)[1] ≈ 7.0
 end
 
 @testset "Optimal control problem" begin
+    # Investing
+    
+   
+    # Bang-bang control
 end
