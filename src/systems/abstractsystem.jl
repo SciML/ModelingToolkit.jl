154bbbd83c39c344ecd686497537c33596484079
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-04-16 17:07:15 +0530

    refactor: remove `generate_factorized_W`

--- src/systems/abstractsystem.jl
+++ src/systems/abstractsystem.jl
@@ -115,17 +115,6 @@ function generate_jacobian end
 
 """
 ```julia
-generate_factorized_W(sys::AbstractSystem, dvs = unknowns(sys), ps = parameters(sys),
-                      expression = Val{true}; sparse = false, kwargs...)
-```
-
-Generates a function for the factorized W matrix of a system. Extra arguments control
-the arguments to the internal [`build_function`](@ref) call.
-"""
-function generate_factorized_W end
-
-"""
-```julia
 generate_hessian(sys::AbstractSystem, dvs = unknowns(sys), ps = parameters(sys),
                  expression = Val{true}; sparse = false, kwargs...)
 ```
