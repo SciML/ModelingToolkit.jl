a4d5fe299f77a6aa7400fcd4836bd0e324830e8d
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-04-16 19:58:55 +0530

    feat: implement `IntervalNonlinearProblem`, `IntervalNonlinearFunction` for `System`

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -172,6 +172,7 @@ include("problems/daeproblem.jl")
 include("problems/sdeproblem.jl")
 include("problems/sddeproblem.jl")
 include("problems/nonlinearproblem.jl")
+include("problems/intervalnonlinearproblem.jl")
 include("systems/optimization/modelingtoolkitize.jl")
 
 include("systems/nonlinear/homotopy_continuation.jl")
