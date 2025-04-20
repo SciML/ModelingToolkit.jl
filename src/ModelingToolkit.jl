b953ee2bf97b69b3724cb85f59f1d18aba92ab71
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-04-20 15:21:23 +0530

    refactor: remove `sdesystem.jl`

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -168,7 +168,6 @@ include("systems/optimization/optimizationsystem.jl")
 include("systems/optimization/modelingtoolkitize.jl")
 
 include("systems/nonlinear/nonlinearsystem.jl")
-include("systems/diffeqs/sdesystem.jl")
 include("systems/diffeqs/abstractodesystem.jl")
 include("systems/nonlinear/homotopy_continuation.jl")
 include("systems/nonlinear/modelingtoolkitize.jl")
@@ -261,7 +260,7 @@ export AbstractTimeDependentSystem,
 export ODEFunction, ODEFunctionExpr, ODEProblemExpr, convert_system,
        add_accumulations, System
 export DAEFunctionExpr, DAEProblemExpr
-export SDESystem, SDEFunction, SDEFunctionExpr, SDEProblemExpr
+export SDEFunction, SDEFunctionExpr, SDEProblemExpr
 export SystemStructure
 export DiscreteSystem, DiscreteProblem, DiscreteFunction, DiscreteFunctionExpr
 export ImplicitDiscreteSystem, ImplicitDiscreteProblem, ImplicitDiscreteFunction,
