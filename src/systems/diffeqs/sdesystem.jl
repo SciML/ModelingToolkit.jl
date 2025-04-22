79278a2f55f8b05dfebf23495f6d63d598688651
vyudu <vincent.duyuan@gmail.com>
2025-04-22 18:52:15 -0400

    fix: fix sfmi bugs and

--- src/systems/diffeqs/sdesystem.jl
+++ src/systems/diffeqs/sdesystem.jl
@@ -272,10 +272,8 @@ function SDESystem(deqs::AbstractVector{<:Equation}, neqs::AbstractArray, iv, dv
 
     alg_eqs = filter(eq -> eq.lhs isa Union{Symbolic, Number} && !is_diff_equation(eq),
         deqs)
-    cont_callbacks = to_cb_vector(SymbolicContinuousCallback.(
-        continuous_events; alg_eqs = alg_eqs, iv = iv, warn_no_algebraic = false))
-    disc_callbacks = to_cb_vector(SymbolicDiscreteCallback.(
-        discrete_events; alg_eqs = alg_eqs, iv = iv, warn_no_algebraic = false))
+    cont_callbacks = to_cb_vector(continuous_events; CB_TYPE = SymbolicContinuousCallback, iv = iv, alg_eqs = alg_eqs, warn_no_algebraic = false)
+    disc_callbacks = to_cb_vector(discrete_events; CB_TYPE = SymbolicDiscreteCallback, iv = iv, alg_eqs = alg_eqs, warn_no_algebraic = false)
 
     if is_dde === nothing
         is_dde = _check_if_dde(deqs, iv′, systems)
