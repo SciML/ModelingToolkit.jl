e4774330528080d703f8cc312847cd20442a3878
vyudu <vincent.duyuan@gmail.com>
2025-04-22 15:18:23 -0400

    fix: require simplification again

--- src/systems/systems.jl
+++ src/systems/systems.jl
@@ -33,6 +33,10 @@ function structural_simplify(
         disturbance_inputs = Any[],
         kwargs...)
     isscheduled(sys) && throw(RepeatedStructuralSimplificationError())
+    if inputs isa Union{Symbol, Vector{Symbol}, AnalysisPoint, Vector{AnalysisPoint}}
+        sys, inputs, outputs = ap_preprocessing(sys, inputs, outputs, kwargs...)
+    end
+
     newsys′ = __structural_simplification(sys; simplify,
         allow_symbolic, allow_parameter, conservative, fully_determined,
         inputs, outputs, disturbance_inputs,
