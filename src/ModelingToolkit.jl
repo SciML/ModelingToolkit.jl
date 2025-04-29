75efe9c98fe559fd01accd0b646093acc21cd472
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-04-29 15:25:04 +0530

    feat: add `modelingtoolkitize` for `SDEProblem`

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -182,6 +182,7 @@ include("problems/bvproblem.jl")
 
 include("modelingtoolkitize/common.jl")
 include("modelingtoolkitize/odeproblem.jl")
+include("modelingtoolkitize/sdeproblem.jl")
 
 include("systems/optimization/modelingtoolkitize.jl")
 
