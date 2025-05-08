45b3d5924da84e0afe55539ff100fe7735281c1a
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-05-08 18:38:12 +0530

    test: update DDE tests to new semantics

--- test/dde.jl
+++ test/dde.jl
@@ -86,7 +86,7 @@ eqs = [D(x(t)) ~ a * x(t) + b * x(t - τ) + c + (α * x(t) + γ) * η, delx ~ x(
 @test ModelingToolkit.is_dde(sys)
 @test !is_markovian(sys)
 @test equations(sys) == [D(x(t)) ~ a * x(t) + b * x(t - τ) + c]
-@test isequal(ModelingToolkit.get_noiseeqs(sys), [α * x(t) + γ])
+@test isequal(ModelingToolkit.get_noise_eqs(sys), [α * x(t) + γ;;])
 prob_mtk = SDDEProblem(sys, [x(t) => 1.0 + t], tspan; constant_lags = (τ,));
 @test_nowarn sol_mtk = solve(prob_mtk, RKMil(), seed = 100)
 
