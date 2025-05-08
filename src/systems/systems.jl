71faa50901de62a38bc1d51ca975903cda596816
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-05-08 18:32:44 +0530

    fix: disallow simplification of jump systems

--- src/systems/systems.jl
+++ src/systems/systems.jl
@@ -60,6 +60,9 @@ function __structural_simplify(sys::AbstractSystem; simplify = false,
     if has_noise_eqs(sys) && get_noise_eqs(sys) !== nothing
         sys = noise_to_brownians(sys; names = :αₘₜₖ)
     end
+    if !isempty(jumps(sys))
+        return sys
+    end
     if isempty(equations(sys)) && !is_time_dependent(sys) && !_iszero(cost(sys))
         return simplify_optimization_system(sys; kwargs..., sort_eqs, simplify)
     end
