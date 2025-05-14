025e319a4114e5b3651554abcaab2b4a5e660b4d
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-05-14 11:50:09 +0530

    feat: implement `DiscreteProblem` and `DiscreteFunction` for `System`
    refactor: move `shift_u0map_forward` to `discreteproblem.jl`

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -176,6 +176,7 @@ include("problems/sddeproblem.jl")
 include("problems/nonlinearproblem.jl")
 include("problems/intervalnonlinearproblem.jl")
 include("problems/implicitdiscreteproblem.jl")
+include("problems/discreteproblem.jl")
 include("systems/optimization/modelingtoolkitize.jl")
 
 include("systems/nonlinear/homotopy_continuation.jl")
