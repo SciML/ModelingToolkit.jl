1dcc454e27527cb757adf49fecc3239394a244a4
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-05-14 13:50:38 +0530

    refactor: do not rely on `ArrayPartition` when unit-checking jumps

--- src/systems/validation.jl
+++ src/systems/validation.jl
@@ -5,6 +5,7 @@ using ..ModelingToolkit: ValidationError,
                          ModelingToolkit, Connection, instream, JumpType, VariableUnit,
                          get_systems,
                          Conditional, Comparison
+using JumpProcesses: MassActionJump, ConstantRateJump, VariableRateJump
 using Symbolics: Symbolic, value, issym, isadd, ismul, ispow
 const MT = ModelingToolkit
 
@@ -231,9 +232,13 @@ function validate(jump::MT.MassActionJump, t::Symbolic; info::String = "")
         ["scaled_rates", "1/(t*reactants^$n))"]; info)
 end
 
-function validate(jumps::ArrayPartition{<:Union{Any, Vector{<:JumpType}}}, t::Symbolic)
+function validate(jumps::Vector{JumpType}, t::Symbolic)
     labels = ["in Mass Action Jumps,", "in Constant Rate Jumps,", "in Variable Rate Jumps,"]
-    all([validate(jumps.x[idx], t, info = labels[idx]) for idx in 1:3])
+    majs = filter(x -> x isa MassActionJump, jumps)
+    crjs = filter(x -> x isa ConstantRateJump, jumps)
+    vrjs = filter(x -> x isa VariableRateJump, jumps)
+    splitjumps = [majs, crjs, vrjs]
+    all([validate(js, t; info) for (js, info) in zip(splitjumps, labels)])
 end
 
 function validate(eq::MT.Equation; info::String = "")
