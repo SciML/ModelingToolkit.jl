49096778f39f943438643217d25182b01c5c71b4
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-05-19 18:05:05 +0530

    test: update test with new simplification result

--- test/nonlinearsystem.jl
+++ test/nonlinearsystem.jl
@@ -296,9 +296,9 @@ sys = structural_simplify(ns; conservative = true)
     # system that contains a chain of observed variables when simplified
     @variables x y z
     eqs = [0 ~ x^2 + 2z + y, z ~ y, y ~ x] # analytical solution x = y = z = 0 or -3
-    @mtkbuild ns = System(eqs) # solve for y with observed chain z -> x -> y
-    @test isequal(expand.(calculate_jacobian(ns)), [3 // 2 + y;;])
-    @test isequal(calculate_hessian(ns), [[1;;]])
+    @mtkbuild ns = System(eqs) # solve for y with observed chain z -> y -> x
+    @test isequal(expand.(calculate_jacobian(ns)), [-3 // 2 - x;;])
+    @test isequal(calculate_hessian(ns), [[-1;;]])
     prob = NonlinearProblem(ns, unknowns(ns) .=> -4.0) # give guess < -3 to reach -3
     sol = solve(prob, NewtonRaphson())
     @test sol[x] ≈ sol[y] ≈ sol[z] ≈ -3
