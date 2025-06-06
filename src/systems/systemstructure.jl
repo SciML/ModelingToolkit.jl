aaf7c579e1a1bbc44532b3910938eef59b14f471
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-06-07 01:08:32 +0530

    fix: do not shift inside `Pre` in `shift_discrete_system`

--- src/systems/systemstructure.jl
+++ src/systems/systemstructure.jl
@@ -544,21 +544,21 @@ function shift_discrete_system(ts::TearingState)
     discvars = OrderedSet()
     eqs = equations(sys)
     for eq in eqs
-        vars!(discvars, eq; op = Union{Sample, Hold})
+        vars!(discvars, eq; op = Union{Sample, Hold, Pre})
     end
     iv = get_iv(sys)
 
     discmap = Dict(k => StructuralTransformations.simplify_shifts(Shift(iv, 1)(k))
     for k in discvars
-    if any(isequal(k), fullvars) && !isa(operation(k), Union{Sample, Hold}))
+    if any(isequal(k), fullvars) && !isa(operation(k), Union{Sample, Hold, Pre}))
 
     for i in eachindex(fullvars)
         fullvars[i] = StructuralTransformations.simplify_shifts(fast_substitute(
-            fullvars[i], discmap; operator = Union{Sample, Hold}))
+            fullvars[i], discmap; operator = Union{Sample, Hold, Pre}))
     end
     for i in eachindex(eqs)
         eqs[i] = StructuralTransformations.simplify_shifts(fast_substitute(
-            eqs[i], discmap; operator = Union{Sample, Hold}))
+            eqs[i], discmap; operator = Union{Sample, Hold, Pre}))
     end
     @set! ts.sys.eqs = eqs
     @set! ts.fullvars = fullvars
