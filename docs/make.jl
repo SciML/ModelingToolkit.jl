cf816c46b23ba36b996ecc10024c55497e3b0132
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-06-09 17:16:47 +0530

    docs: add `MTKFMIExt` to modules for doc build

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,8 @@ using ModelingToolkit: SciMLBase
 # To load docstring from extension
 import FMI, CommonSolve, JumpProcesses
 
+MTKFMIExt = Base.get_extension(ModelingToolkit, :MTKFMIExt)
+
 # Make sure that plots don't throw a bunch of warnings / errors!
 ENV["GKSwstype"] = "100"
 using Plots
@@ -24,7 +26,7 @@ mathengine = MathJax3(Dict(:loader => Dict("load" => ["[tex]/require", "[tex]/ma
 
 makedocs(sitename = "ModelingToolkit.jl",
     authors = "Chris Rackauckas",
-    modules = [ModelingToolkit],
+    modules = [ModelingToolkit, MTKFMIExt],
     clean = true, doctest = false, linkcheck = true,
     warnonly = [:docs_block, :missing_docs, :cross_references],
     linkcheck_ignore = [
