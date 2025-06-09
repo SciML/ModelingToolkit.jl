c84b996482e1a117665e66e68092ef15542623de
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-06-09 15:13:28 +0530

    refactor: export `InitializationProblem`

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -330,7 +330,7 @@ export toexpr, get_variables
 export simplify, substitute
 export build_function
 export modelingtoolkitize
-export generate_initializesystem, Initial, isinitial
+export generate_initializesystem, Initial, isinitial, InitializationProblem
 
 export alg_equations, diff_equations, has_alg_equations, has_diff_equations
 export get_alg_eqs, get_diff_eqs, has_alg_eqs, has_diff_eqs
