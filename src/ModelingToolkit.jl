4546474345c966839bba2b6a8fffd7010ce1d0f1
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-06-16 13:41:48 +0530

    feat: add `SemilinearODEFunction` and `SemilinearODEProblem`

--- src/ModelingToolkit.jl
+++ src/ModelingToolkit.jl
@@ -99,6 +99,9 @@ const DQ = DynamicQuantities
 import DifferentiationInterface as DI
 using ADTypes: AutoForwardDiff
 import SciMLPublic: @public
+import PreallocationTools
+import PreallocationTools: DiffCache
+import FillArrays
 
 export @derivatives
 
@@ -288,6 +291,7 @@ export IntervalNonlinearProblem
 export OptimizationProblem, constraints
 export SteadyStateProblem
 export JumpProblem
+export SemilinearODEFunction, SemilinearODEProblem
 export alias_elimination, flatten
 export connect, domain_connect, @connector, Connection, AnalysisPoint, Flow, Stream,
        instream
