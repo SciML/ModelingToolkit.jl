196a96718260effc5fb1d322395ec3b1ba404dc8
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-07-09 12:40:03 +0530

    refactor: improve error message for missing inputs/disturbance inputs/outputs

--- src/linearization.jl
+++ src/linearization.jl
@@ -565,6 +565,51 @@ function linearize_symbolic(sys::AbstractSystem, inputs,
     (; A, B, C, D, f_x, f_z, g_x, g_z, f_u, g_u, h_x, h_z, h_u), sys
 end
 
+struct IONotFoundError <: Exception
+    variant::String
+    sysname::Symbol
+    not_found::Any
+end
+
+function Base.showerror(io::IO, err::IONotFoundError)
+    println(io,
+        "The following $(err.variant) provided to `mtkcompile` were not found in the system:")
+    maybe_namespace_issue = false
+    for var in err.not_found
+        println(io, "  ", var)
+        if hasname(var) && startswith(string(getname(var)), string(err.sysname))
+            maybe_namespace_issue = true
+        end
+    end
+    if maybe_namespace_issue
+        println(io, """
+        Some of the missing variables are namespaced with the name of the system \
+        `$(err.sysname)` passed to `mtkcompile`. This may be indicative of a namespacing \
+        issue. `mtkcompile` requires that the $(err.variant) provided are not namespaced \
+        with the name of the root system. This issue can occur when using `getproperty` \
+        to access the variables passed as $(err.variant). For example:
+
+        ```julia
+        @named sys = MyModel()
+        inputs = [sys.input_var]
+        mtkcompile(sys; inputs)
+        ```
+
+        Here, `mtkcompile` expects the input to be named `input_var`, but since `sys`
+        performs namespacing, it will be named `sys$(NAMESPACE_SEPARATOR)input_var`. To \
+        fix this issue, namespacing can be temporarily disabled:
+
+        ```julia
+        @named sys = MyModel()
+        sys = toggle_namespacing(sys, false)
+        inputs = [sys.input_var]
+        sys = toggle_namespacing(sys, true)
+        mtkcompile(sys; inputs)
+        ```
+        """)
+    end
+end
+
 """
 Modify the variable metadata of system variables to indicate which ones are inputs, outputs, and disturbances. Needed for `inputs`, `outputs`, `disturbances`, `unbound_inputs`, `unbound_outputs` to return the proper subsets.
 """
@@ -605,19 +650,16 @@ function markio!(state, orig_inputs, inputs, outputs, disturbances; check = true
     if check
         ikeys = keys(filter(!last, inputset))
         if !isempty(ikeys)
-            error(
-                "Some specified inputs were not found in system. The following variables were not found ",
-                ikeys)
+            throw(IONotFoundError("inputs", nameof(state.sys), ikeys))
         end
         dkeys = keys(filter(!last, disturbanceset))
         if !isempty(dkeys)
-            error(
-                "Specified disturbance inputs were not found in system. The following variables were not found ",
-                ikeys)
+            throw(IONotFoundError("disturbance inputs", nameof(state.sys), ikeys))
         end
-        (all(values(outputset)) || error(
-            "Some specified outputs were not found in system. The following Dict indicates the found variables ",
-            outputset))
+        okeys = keys(filter(!last, outputset))
+        if !isempty(okeys)
+            throw(IONotFoundError("outputs", nameof(state.sys), okeys))
+        end
     end
     state, orig_inputs
 end
