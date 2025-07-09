4efd06f79ec152b65d538ea62f92099d626db378
fchen121 <fchen121@mit.edu>
2025-07-08 22:19:02 -0400

    Improve test cases

--- test/fractional_to_ordinary.jl
+++ test/fractional_to_ordinary.jl
@@ -7,6 +7,7 @@ using Test
 @variables x(t)
 D = Differential(t)
 tspan = (0., 1.)
+timepoint = [0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
 
 function expect(t, α)
     return (3/2*t^(α/2) - t^4)^2
@@ -18,7 +19,7 @@ eqs += (gamma(9)*t^(8 - α)/gamma(9 - α)) + (3/2*t^(α/2)-t^4)^3 - x^(3/2)
 sys = fractional_to_ordinary(eqs, x, α, 10^-7, 1)
 
 prob = ODEProblem(sys, [], tspan)
-sol = solve(prob, radau5(), abstol = 1e-10, reltol = 1e-10)
+sol = solve(prob, radau5(), saveat=timepoint, abstol = 1e-10, reltol = 1e-10)
 
 time = 0
 while(time <= 1)
@@ -32,11 +33,11 @@ eqs += (gamma(9)*t^(8 - α)/gamma(9 - α)) + (3/2*t^(α/2)-t^4)^3 - x^(3/2)
 sys = fractional_to_ordinary(eqs, x, α, 10^-7, 1)
 
 prob = ODEProblem(sys, [], tspan)
-sol = solve(prob, radau5(), abstol = 1e-10, reltol = 1e-10)
+sol = solve(prob, radau5(), saveat=timepoint, abstol = 1e-10, reltol = 1e-10)
 
 time = 0
 while(time <= 1)
-    @test isapprox(expect(time, α), sol(time, idxs=x), atol=1e-3)
+    @test isapprox(expect(time, α), sol(time, idxs=x), atol=1e-7)
     time += 0.1
 end
 
@@ -46,11 +47,11 @@ eqs += (gamma(9)*t^(8 - α)/gamma(9 - α)) + (3/2*t^(α/2)-t^4)^3 - x^(3/2)
 sys = fractional_to_ordinary(eqs, x, α, 10^-7, 1)
 
 prob = ODEProblem(sys, [], tspan)
-sol = solve(prob, radau5(), abstol = 1e-10, reltol = 1e-10)
+sol = solve(prob, radau5(), saveat=timepoint, abstol = 1e-10, reltol = 1e-10)
 
 time = 0
 while(time <= 1)
-    @test isapprox(expect(time, α), sol(time, idxs=x), atol=1e-3)
+    @test isapprox(expect(time, α), sol(time, idxs=x), atol=1e-7)
     time += 0.1
 end
 
@@ -60,9 +61,25 @@ end
 D = Differential(t)
 tspan = (0., 220.)
 
-sys = fractional_to_ordinary([1 - 4*x + x^2 * y, 3*x - x^2 * y], [x, y], [1.3, 0.8], 10^-6, 220; initials=[[1.2, 1], 2.8])
+sys = fractional_to_ordinary([1 - 4*x + x^2 * y, 3*x - x^2 * y], [x, y], [1.3, 0.8], 10^-8, 220; initials=[[1.2, 1], 2.8])
 prob = ODEProblem(sys, [], tspan)
 sol = solve(prob, radau5(), abstol = 1e-8, reltol = 1e-8)
 
-@test isapprox(1.0097684171, sol(220, idxs=x), atol=1e-3)
-@test isapprox(2.1581264031, sol(220, idxs=y), atol=1e-3)
+@test isapprox(1.0097684171, sol(220, idxs=x), atol=1e-5)
+@test isapprox(2.1581264031, sol(220, idxs=y), atol=1e-5)
+
+#Testing for example 3 of Section 7
+@independent_variables t
+@variables x_0(t)
+D = Differential(t)
+tspan = (0., 5000.)
+
+function expect(t)
+    return sqrt(2) * sin(t + pi/4)
+end
+
+sys = linear_fractional_to_ordinary([3, 2.5, 2, 1, .5, 0], [1, 1, 1, 4, 1, 4], 6*cos(t), 10^-5, 5000; initials=[1, 1, -1])
+prob = ODEProblem(sys, [], tspan)
+sol = solve(prob, radau5(), abstol = 1e-5, reltol = 1e-5)
+
+@test isapprox(expect(5000), sol(5000, idxs=x_0), atol=1e-6)
