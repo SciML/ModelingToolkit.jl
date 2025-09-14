2d088e65605ece53ccd9eb424e0ef8dc9e26e01f
Brad Carman <bradleygcarman@outlook.com>
2025-09-14 19:25:11 -0400

    unbound_inputs

--- src/inputs.jl
+++ src/inputs.jl
@@ -8,87 +8,94 @@ struct Input
     time::SVector
 end
 
-function DiffEqBase.solve(prob::SciMLBase.AbstractDEProblem, inputs::Union{Input, Vector{Input}}, args...; input_funs, kwargs...)
+function Input(var, data::Vector{<:Real}, time::Vector{<:Real}) 
+    n = length(data)
+    return Input(var, SVector{n}(data), SVector{n}(time))
+end
 
-    set_input!, finalize! = input_funs
+struct InputFunctions
+    events::Tuple
+    vars::Tuple
+    setters::Tuple
+end
+
+InputFunctions(events::Vector, vars::Vector, setters::Vector) = InputFunctions(Tuple(events), Tuple(vars), Tuple(setters))
+
+function set_input!(input_funs::InputFunctions, integrator, var, value::Real) 
+    i = findfirst(isequal(var), input_funs.vars)
+    setter = input_funs.setters[i]
+    event = input_funs.events[i]
+    
+    setter(integrator, value)           
+    save_callback_discretes!(integrator, event)
+    u_modified!(integrator, true)
+    return nothing
+end
+
+function finalize!(input_funs::InputFunctions, integrator) 
+
+    for i in eachindex(input_funs.vars)
+        save_callback_discretes!(integrator, input_funs.events[i])
+    end
+
+    return nothing
+end
+
+(input_funs::InputFunctions)(integrator, var, value::Real) = set_input!(input_funs, integrator, var, value)
+(input_funs::InputFunctions)(integrator) = finalize!(input_funs, integrator)
+
+function setup_inputs(sys, inputs = unbound_inputs(sys))
+    
+    vars = SymbolicUtils.BasicSymbolic[isparameter(x) ? x : toparam(x) for x in unwrap.(inputs)]
+    setters = []
+    events = SymbolicDiscreteCallback[]
+    if !isempty(vars)
+        
+        for x in vars
+            affect = ImperativeAffect((m, o, c, i)->m, modified=(;x))
+            sdc = SymbolicDiscreteCallback(Inf, affect)
+
+            push!(events, sdc)
+        end
+
+        @set! sys.discrete_events = events
+        @set! sys.index_cache = ModelingToolkit.IndexCache(sys)
+
+        setters = [SymbolicIndexingInterface.setsym(sys, x) for x in vars]
+        
+    end
+
+    return sys, InputFunctions(events, vars, setters)
+end
+
+
+
+
+function DiffEqBase.solve(prob::SciMLBase.AbstractDEProblem, inputs::Vector{Input}, args...; input_funs::InputFunctions, kwargs...)
 
     tstops = Float64[]
     callbacks = DiscreteCallback[]
-    if !isa(inputs, Vector)
-        inputs = [inputs]
-    end
 
     for input::Input in inputs
+
         tstops = union(tstops, input.time)
-        
         condition = (u,t,integrator) -> any(t .== input.time)
         affect! = function (integrator) 
-            i = findfirst(integrator.t .== input.time)
-            set_input!(integrator, input.var, input.data[i])
+            @inbounds begin
+                i = findfirst(integrator.t .== input.time)
+                input_funs(integrator, input.var, input.data[i])
+            end 
         end
-        callback = DiscreteCallback(condition, affect!)
-
-        push!(callbacks, callback)
+        push!(callbacks, DiscreteCallback(condition, affect!))
+    
     end
 
     # finalize!
     t_end = prob.tspan[2]
     condition = (u,t,integrator) -> (t == t_end)
-    affect! = (integrator) -> finalize!(integrator)
-    callback = DiscreteCallback(condition, affect!)
-
-    push!(callbacks, callback)
+    affect! = (integrator) -> input_funs(integrator)
+    push!(callbacks, DiscreteCallback(condition, affect!))
     push!(tstops, t_end)
 
     return solve(prob, args...; tstops, callback=CallbackSet(callbacks...), kwargs...)
 end
-
-function setup_inputs(sys)
-
-    inputs = ModelingToolkit.unbound_inputs(sys)
-    setters = Dict{Num, Function}()
-
-    if !isempty(inputs)
-        sdcs = ModelingToolkit.SymbolicDiscreteCallback[]
-        for x in inputs
-            affect = ModelingToolkit.ImperativeAffect((m, o, c, i)->m, modified=(;x))
-            sdc = ModelingToolkit.SymbolicDiscreteCallback(Inf, affect)
-
-            push!(sdcs, sdc)
-        end
-
-        @set! sys.discrete_events = sdcs
-        sys = complete(sys)  # @set! sys.index_cache = ModelingToolkit.IndexCache(sys)
-
-        for (i,x) in enumerate(inputs)
-            setter = SymbolicIndexingInterface.setsym(sys, x)
-            sdc = sdcs[i]
-
-            setval = function (integrator, set, val=NaN)
-                if set
-                    println("::setting $x to $val @ $(integrator.t)s")
-                    setter(integrator, val)
-                else
-                    println("::saving $x @ $(integrator.t)s")
-                end
-                ModelingToolkit.save_callback_discretes!(integrator, sdc)
-            end
-
-            setters[x] = setval
-        end
-    end
-
-    set_input! = function (integrator, var, value) 
-        setters[var](integrator, true, value)
-        u_modified!(integrator, true)
-    end
-
-    finalize! = function (integrator) 
-        for ky in keys(setters)
-            setters[ky](integrator, false)
-        end
-    end
-
-    return sys, set_input!, finalize!
-end
-
