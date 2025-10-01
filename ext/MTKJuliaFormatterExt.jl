99ebed168aade368954cf60b26f8bce90f63111b
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2025-10-01 16:48:20 +0530

    fix: move JuliaFormatter to an extension

---
+++ ext/MTKJuliaFormatterExt.jl
@@ -0,0 +1,12 @@
+module MTKJuliaFormatterExt
+
+import ModelingToolkit: readable_code, _readable_code, rec_remove_macro_linenums!
+import JuliaFormatter
+
+function readable_code(expr::Expr)
+    expr = Base.remove_linenums!(_readable_code(expr))
+    rec_remove_macro_linenums!(expr)
+    JuliaFormatter.format_text(string(expr), JuliaFormatter.SciMLStyle())
+end
+
+end
