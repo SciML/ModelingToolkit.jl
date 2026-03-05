383672f9fada15cd7ae783a60866f1513053ce4c
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2026-03-05 17:19:35 +0530

    test: do not run `InterfaceII` inside a testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -67,23 +67,21 @@ end
     end
 
     if GROUP == "All" || GROUP == "InterfaceII"
-        @testset "InterfaceII" begin
-            @mtktestset("Code Generation Test", "code_generation.jl")
-            @mtktestset("Discrete System", "discrete_system.jl")
-            @mtktestset("Implicit Discrete System", "implicit_discrete_system.jl")
-            @mtktestset("SDESystem Test", "sdesystem.jl")
-            @mtktestset("DDESystem Test", "dde.jl")
-            @mtktestset("NonlinearSystem Test", "nonlinearsystem.jl")
-            @safetestset "SCCNonlinearProblem Test" include("scc_nonlinear_problem.jl")
-            @safetestset "IfLifting Test" include("if_lifting.jl")
-            @mtktestset("Analysis Points Test", "analysis_points.jl")
-            @mtktestset("Causal Variables Connection Test", "causal_variables_connection.jl")
-            @safetestset "Subsystem replacement" include("substitute_component.jl")
-            @safetestset "Linearization Tests" include("linearize.jl")
-            @safetestset "Fractional Differential Equations Tests" include("fractional_to_ordinary.jl")
-            @safetestset "SemilinearODEProblem tests" include("semilinearodeproblem.jl")
-            @mtktestset("OptimizationSystem Test", "optimizationsystem.jl")
-        end
+        @mtktestset("Code Generation Test", "code_generation.jl")
+        @mtktestset("Discrete System", "discrete_system.jl")
+        @mtktestset("Implicit Discrete System", "implicit_discrete_system.jl")
+        @mtktestset("SDESystem Test", "sdesystem.jl")
+        @mtktestset("DDESystem Test", "dde.jl")
+        @mtktestset("NonlinearSystem Test", "nonlinearsystem.jl")
+        @safetestset "SCCNonlinearProblem Test" include("scc_nonlinear_problem.jl")
+        @safetestset "IfLifting Test" include("if_lifting.jl")
+        @mtktestset("Analysis Points Test", "analysis_points.jl")
+        @mtktestset("Causal Variables Connection Test", "causal_variables_connection.jl")
+        @safetestset "Subsystem replacement" include("substitute_component.jl")
+        @safetestset "Linearization Tests" include("linearize.jl")
+        @safetestset "Fractional Differential Equations Tests" include("fractional_to_ordinary.jl")
+        @safetestset "SemilinearODEProblem tests" include("semilinearodeproblem.jl")
+        @mtktestset("OptimizationSystem Test", "optimizationsystem.jl")
     end
 
     if GROUP == "All" || GROUP == "SymbolicIndexingInterface"
