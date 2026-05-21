86b613ed9ea98d6273ca4bc7cac01694e5d5bef5
lf <yanuling0@gmail.com>
2026-05-21 14:35:18 +0800

    test: Q2 nested homotopy collapses to innermost actual

--- lib/ModelingToolkitBase/test/homotopy_init.jl
+++ lib/ModelingToolkitBase/test/homotopy_init.jl
@@ -12,4 +12,20 @@ using Symbolics
         @test isequal(Symbolics.unwrap(rewritten), Symbolics.unwrap(x^2 - p))
         @test !has_homotopy(rewritten)
     end
+
+    @testset "Q2 nested" begin
+        @variables x
+        @parameters p
+        inner = homotopy(x^2 - p, x - sqrt(p))
+        outer = homotopy(inner, x - 1)
+        @test isequal(Symbolics.unwrap(rewrite_trivial(outer)),
+                      Symbolics.unwrap(x^2 - p))
+
+        triple = homotopy(homotopy(homotopy(x, x + 1), x + 2), x + 3)
+        @test isequal(Symbolics.unwrap(rewrite_trivial(triple)),
+                      Symbolics.unwrap(x))
+
+        @test !has_homotopy(rewrite_trivial(outer))
+        @test !has_homotopy(rewrite_trivial(triple))
+    end
 end
