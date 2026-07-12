39c88bcce2b81f5d431801bd3306835b882ca9e1
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-07-12 07:30:50 -0400

    Align downstream LinearSolve compatibility
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- test/qa/qa.jl
+++ test/qa/qa.jl
@@ -2,6 +2,16 @@ using ModelingToolkit
 using Aqua
 using JET
 using SciMLTesting
+using TOML
+using Test
+
+@testset "Downstream LinearSolve compatibility" begin
+    root_project = TOML.parsefile(joinpath(@__DIR__, "..", "..", "Project.toml"))
+    downstream_project = TOML.parsefile(joinpath(@__DIR__, "..", "downstream", "Project.toml"))
+
+    @test downstream_project["deps"]["LinearSolve"] == root_project["extras"]["LinearSolve"]
+    @test downstream_project["compat"]["LinearSolve"] == root_project["compat"]["LinearSolve"]
+end
 
 run_qa(
     ModelingToolkit;
